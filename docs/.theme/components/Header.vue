5dcc0d5925275d71ef131c0ea48783dc1618a028
cihandeniz <cihan@gcbrains.com>
2023-06-08 18:13:17 +0300

    make header responsive

--- docs/.theme/components/Header.vue
+++ docs/.theme/components/Header.vue
@@ -6,18 +6,28 @@
           <img class="do logo">
         </NuxtLink>
       </div>
-      <nav>
+      <a
+        class="bars"
+        @click="toggle"
+      ><i class="fa-solid fa-bars" /></a>
+      <nav :class="{ active: menuShown }">
+        <a
+          class="close"
+          @click="toggle"
+        ><i class="fa-solid fa-close" /></a>
         <NuxtLink
           v-for="menu in menus"
           :key="menu.title"
           :to="menu._path"
           :class="{ active: menu._path === root }"
+          @click="close"
         >
           {{ menu.title }}
         </NuxtLink>
         <NuxtLink
           :to="`https://github.com${runtimeConfig.public.githubURL}`"
           target="_blank"
+          @click="close"
         >
           <i class="fa-brands fa-github" />
         </NuxtLink>
@@ -26,7 +36,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { useRoute, useRuntimeConfig } from "#imports";
+import { useRoute, useRuntimeConfig, ref } from "#imports";
 
 const runtimeConfig = useRuntimeConfig();
 const route = useRoute();
@@ -37,6 +47,10 @@ const menus = await queryContent("/")
   .where({ _dir: { $eq: "" }, _path: { $ne: "/" }, position: { $exists: true } })
   .sort({ position: 1, $numeric: true })
   .find();
+
+const menuShown = ref<boolean>(false);
+function toggle() { menuShown.value = !menuShown.value; }
+function close() { menuShown.value = false; }
 </script>
 <style lang="scss" scoped>
 div.top {
@@ -52,6 +66,10 @@ header {
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+
+  .bars {
+    display: none;
+  }
 }
 
 div.logo {
@@ -70,19 +88,73 @@ div.logo {
   }
 }
 
-nav a {
-  margin: 10px;
-  text-decoration: none;
+a {
   color: $color-fg-passive;
+  cursor: pointer;
 
   &:hover {
     color: $color-brand;
+  }
+}
+
+nav a {
+  margin: 10px;
+  text-decoration: none;
+
+  &.close {
+    display: none;
   }
 
   &.active {
     @include border(bottom);
     border-bottom-color: $color-brand;
     padding-bottom: 22px;
+  }
+}
+
+@media (max-width: $width-page-l) {
+  a.bars {
+    display: block;
+  }
+
+  nav {
+    position: fixed;
+    top: 0px;
+    right: 0;
+    z-index: 1;
+    background: black;
+    height: 100%;
+    width: 320px;
+    padding: 20px;
+    border-left: solid 2px #222222;
+    display: none;
+
+    &.active {
+      display: block;
+    }
+
+    a {
+      display: block;
+      height: 2em;
+
+      &.close {
+        display: block;
+        margin-bottom: 1em;
+
+        i {
+          font-size: larger;
+        }
+      }
+
+      &.active {
+        border: 0;
+        @include border(left);
+        border-left-color: $color-brand;
+        padding-bottom: 0;
+        padding-left: 29px;
+        margin-left: -22px;
+      }
+    }
   }
 }
 </style>
