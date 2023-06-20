9a9a0ec504a9bd69df753376618e58d0eac4c9af
cihandeniz <cihan@gcbrains.com>
2023-06-20 14:40:55 +0300

    init do-architecture feature

--- test/blueprints/Do.Blueprints.Service.Test/Program.cs
+++ test/blueprints/Do.Blueprints.Service.Test/Program.cs
@@ -4,3 +4,36 @@ builder.Services.AddDo(with => with.Service());
 var app = builder.Build();
 app.UseDo();
 app.Run();
+
+/**
+Build.Application
+    .As(app =>
+    {
+        app.Layers.AddDomain();
+        app.Layers.AddMonitoring();
+        app.Layers.AddRdbms();
+
+        app.Features.AddAuthentication(c => c.JwtBearer());
+        app.Features.AddLogging(c => c.Default());
+        app.Features.AddDomainObjects(c => c.UseAssemblies("Do.Domain.Test*"));
+    })
+    .Run();
+
+Build.Application
+    .AsService(
+        authentication: c => c.JwtBearer(),
+        custom: app =>
+        {
+            app.Layers.AddCustom();
+
+            app.Features.AddCustom();
+        }
+    )
+    .Run();
+
+Build.Application
+    .AsGateway(
+        gateway: c => c.Default(allow: "*", deny: "Admin.*")
+    )
+    .Run();
+*/
---
+++ unreleased.md
@@ -0,0 +1,7 @@
+# Unreleased
+
+## Feature
+
+- `Do.Architecture` package is introduced. This package exposes core _feature_
+  and _layer_ system that is mentioned in
+  [Architecture](../architecture/README.md) section.
