8ab26e87aad042c3d006c8a11176ea510aced1ad
cihandeniz <cihan@gcbrains.com>
2023-07-08 14:22:01 +0300

    minor revision

--- test/core/Do.Test.Architecture/Architecture/Application/ForgingNewApplication.cs
+++ test/core/Do.Test.Architecture/Architecture/Application/ForgingNewApplication.cs
@@ -3,17 +3,17 @@
 public class ForgingNewApplication : Spec
 {
     [Test]
-    public void Builder_is_accessible_via_a_fluent_api()
+    public void It_is_accessible_via_a_fluent_api()
     {
         Assert.That(Forge.New, Is.InstanceOf<Forge>());
     }
 
     [Test]
-    public void Application_is_built_through_the_builder()
+    public void Application_is_forged_through_the_builder_object()
     {
-        var forgeNew = GiveMe.AForge();
+        var forge = GiveMe.AForge();
 
-        var actual = forgeNew.Application(_ => { });
+        var actual = forge.Application(_ => { });
 
         Assert.That(actual, Is.InstanceOf<Do.Architecture.Application>());
     }
