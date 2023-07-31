8d2144127622ac8229827073ef185b161fed933b
Sefer Mirza <sefermirza@gmail.com>
2023-07-31 10:21:18 +0300

    fix link routing with hash

--- docs/.theme/components/Prose/ProseA.vue
+++ docs/.theme/components/Prose/ProseA.vue
@@ -1,22 +1,28 @@
 <template>
   <NuxtLink
-    :href="href"
-    :target="href.startsWith('http') ? '_blank' : target"
+    :href="manipulatedHref"
+    :target="manipulatedHref.startsWith('http') ? '_blank' : target"
   >
-    <slot /><i
-      v-if="href.startsWith('http')"
+    <slot />
+    <i
+      v-if="manipulatedHref.startsWith('http')"
       class="fa-solid fa-arrow-up-right-from-square"
     />
   </NuxtLink>
 </template>
 <script setup lang="ts">
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
   href: string,
   target?: string
 }>(), {
   href: "",
   target: undefined
 });
+
+const manipulatedHref = props.href
+  .replace("/index.md#", "#")
+  .replace("index.md#", "#")
+  .replace(".md#", "#");
 </script>
 <style lang="scss" scoped>
 a {
