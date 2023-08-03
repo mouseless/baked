b599a6f6e0c2fa11dfe23c195bdfea03fed674a1
dncsvr <disever10@alm.ku.edu.tr>
2023-08-04 01:52:16 +0300

    add configure applicationparts extensions

--- src/blueprints/Do.Blueprints.Service/RestApi/RestApiExtensions.cs
+++ src/blueprints/Do.Blueprints.Service/RestApi/RestApiExtensions.cs
@@ -9,7 +9,7 @@ namespace Do;
 public static class RestApiExtensions
 {
     public static void AddRestApi(this IList<ILayer> source) => source.Add(new RestApiLayer());
-
+    public static void ConfigureApplicationParts(this LayerConfigurator source, Action<IApplicationPartCollection> configuration) => source.Configure(configuration);
     public static void ConfigureSwaggerGenOptions(this LayerConfigurator source, Action<SwaggerGenOptions> configuration) => source.Configure(configuration);
     public static void ConfigureSwaggerOptions(this LayerConfigurator source, Action<SwaggerOptions> configuration) => source.Configure(configuration);
     public static void ConfigureSwaggerUIOptions(this LayerConfigurator source, Action<SwaggerUIOptions> configuration) => source.Configure(configuration);
