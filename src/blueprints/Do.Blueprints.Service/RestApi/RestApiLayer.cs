ca18049e14171d34aa0ba4d166f1e5d9f1eb6bb0
dncsvr <disever10@alm.ku.edu.tr>
2023-08-04 02:24:38 +0300

    reivew changes

--- src/blueprints/Do.Blueprints.Service/RestApi/RestApiLayer.cs
+++ src/blueprints/Do.Blueprints.Service/RestApi/RestApiLayer.cs
@@ -24,7 +24,6 @@ public class RestApiLayer : LayerBase<AddServices, Build>
         services.AddHttpContextAccessor();
         services.AddMvcCore().AddApiExplorer();
         services.AddSwaggerGen();
-        services.AddHttpContextAccessor();
 
         return phase.CreateContextBuilder()
             .Add(_applicationParts)
