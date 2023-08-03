38a933dbc32cee490c0ee6a711f369fbad93568d
alptuncs <alptuncsever@gmail.com>
2023-08-03 13:51:14 +0300

    take list as parameter in add dependency injection

--- src/blueprints/Do.Blueprints.Service/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/blueprints/Do.Blueprints.Service/DependencyInjection/DependencyInjectionExtensions.cs
@@ -6,6 +6,6 @@ namespace Do;
 
 public static class DependencyInjectionExtensions
 {
-    public static void AddDependencyInjection(this ICollection<ILayer> layers) => layers.Add(new DependencyInjectionLayer());
+    public static void AddDependencyInjection(this List<ILayer> layers) => layers.Add(new DependencyInjectionLayer());
     public static void ConfigureServiceCollection(this LayerConfigurator configurator, Action<IServiceCollection> configuration) => configurator.Configure(configuration);
 }
