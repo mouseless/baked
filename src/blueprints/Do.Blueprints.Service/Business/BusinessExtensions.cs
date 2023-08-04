565d9814096e126900450edae1ae67a05fc04a80
dncsvr <disever10@alm.ku.edu.tr>
2023-08-04 03:33:50 +0300

    add business feature

---
+++ src/blueprints/Do.Blueprints.Service/Business/BusinessExtensions.cs
@@ -0,0 +1,16 @@
+using Do.Architecture;
+using Do.Business;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Do;
+
+public static class BusinessExtensions
+{
+    public static void AddBusiness(this List<IFeature> source, Func<BusinessConfigurator, IFeature> configure) => source.Add(configure(new()));
+
+    public static void AddTransientWithFactory<T>(this IServiceCollection source) where T : class
+    {
+        source.AddSingleton<Func<T>>(sp => () => sp.GetRequiredServiceUsingRequestServices<T>());
+        source.AddTransient<T>();
+    }
+}
