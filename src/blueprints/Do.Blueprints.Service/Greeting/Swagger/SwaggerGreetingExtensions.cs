0ee4f67d4562d959601eeb80604efc5e00d936cd
dncsvr <disever10@alm.ku.edu.tr>
2023-08-04 03:34:11 +0300

    add greeting feature swagger implementation

---
+++ src/blueprints/Do.Blueprints.Service/Greeting/Swagger/SwaggerGreetingExtensions.cs
@@ -0,0 +1,9 @@
+using Do.Greeting;
+using Do.Greeting.Swagger;
+
+namespace Do;
+
+public static class SwaggerGreetingExtensions
+{
+    public static SwaggerGreetingFeature Swagger(this GreetingConfigurator source) => new();
+}
---
+++ src/blueprints/Do.Blueprints.Service/Greeting/Swagger/SwaggerGreetingFeature.cs
@@ -0,0 +1,20 @@
+using Do.Architecture;
+using Microsoft.AspNetCore.Builder;
+
+namespace Do.Greeting.Swagger;
+
+public class SwaggerGreetingFeature : IFeature
+{
+    public void Configure(LayerConfigurator configurator)
+    {
+        configurator.ConfigureEndpointRouteBuilder(endpoints =>
+        {
+            endpoints.MapGet("/", context =>
+            {
+                context.Response.Redirect("/swagger/index.html");
+
+                return Task.CompletedTask;
+            });
+        });
+    }
+}
