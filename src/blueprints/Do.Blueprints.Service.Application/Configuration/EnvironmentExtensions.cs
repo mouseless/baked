84e82da0ddfe8c3c9162c0bfd32b572ac71fa05c
alptuncs <alptuncsever@gmail.com>
2023-08-08 15:17:18 +0300

    Fix indentation and using order in EnvironmentExtensions

--- src/blueprints/Do.Blueprints.Service.Application/Configuration/EnvironmentExtensions.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Configuration/EnvironmentExtensions.cs
@@ -1,18 +1,18 @@
-using Microsoft.Extensions.Hosting;
 using Do.Architecture;
+using Microsoft.Extensions.Hosting;
 
 namespace Do;
 
 public static class EnvironmentExtensions
 {
     public static IFeature ForDevelopment(this IFeature @default, IFeature featureOnDevelopment) =>
-      @default.For(Environments.Development, featureOnDevelopment);
+        @default.For(Environments.Development, featureOnDevelopment);
 
     public static IFeature ForStaging(this IFeature @default, IFeature featureOnStaging) =>
-      @default.For(Environments.Staging, featureOnStaging);
+        @default.For(Environments.Staging, featureOnStaging);
 
     public static IFeature ForProduction(this IFeature @default, IFeature featureOnProduction) =>
-      @default.For(Environments.Production, featureOnProduction);
+        @default.For(Environments.Production, featureOnProduction);
 
     public static IFeature For(this IFeature @default, string environment, IFeature featureOnEnvironment)
     {
