2f2b80f6352e30c21c37d12cde630c7bc3d87ac9
alptuncs <alptuncsever@gmail.com>
2023-08-14 16:31:42 +0300

    add empty context case to exception

--- src/core/Do.Architecture/Architecture/NotFoundException.cs
+++ src/core/Do.Architecture/Architecture/NotFoundException.cs
@@ -2,8 +2,8 @@
 
 public class NotFoundException : Exception
 {
-    public NotFoundException(string message)
-        : base(
+    public NotFoundException(string? message)
+        : base( message: message == null ? "Context is empty" : 
             "Given type could not be found in ApplicationContext." +
             $" Did you mean ? {message}"
         )
