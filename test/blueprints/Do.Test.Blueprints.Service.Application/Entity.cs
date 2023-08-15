7607271d04c0ba35fd0793d6829db7f5abc3a5db
cihandeniz <cihan@gcbrains.com>
2023-08-15 14:48:29 +0300

    test transactional entity action
    - add docker compose to makefile

--- test/blueprints/Do.Test.Blueprints.Service.Application/Entity.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Application/Entity.cs
@@ -1,14 +1,16 @@
-﻿using Do.Orm;
+﻿using Do.Database;
+using Do.Orm;
 
 namespace Do.Test;
 
 public class Entity
 {
     readonly IEntityContext<Entity> _context = default!;
+    readonly ITransaction _transaction = default!;
 
     protected Entity() { }
-    public Entity(IEntityContext<Entity> context) =>
-        _context = context;
+    public Entity(IEntityContext<Entity> context, ITransaction transaction) =>
+        (_context, _transaction) = (context, transaction);
 
     public virtual Guid Id { get; protected set; } = default!;
     public virtual Guid Guid { get; protected set; } = default!;
@@ -20,17 +22,23 @@ public class Entity
 
     public virtual Entity With(Guid guid, string @string, string stringData, int int32, Uri uri, object @dynamic)
     {
-        Guid = guid;
-        String = @string;
-        StringData = stringData;
-        Int32 = int32;
-        Uri = uri;
-        Dynamic = @dynamic;
+        Set(guid, @string, stringData, int32, uri, @dynamic);
 
         return _context.Insert(this);
     }
 
-    public virtual void Update(Guid guid, string @string, string stringData, int int32, Uri uri, object @dynamic)
+    public virtual async Task Update(Guid guid, string @string, string stringData, int int32, Uri uri, object @dynamic,
+        bool useTransaction = false,
+        bool throwError = false
+    )
+    {
+        if (useTransaction) { await _transaction.CommitAsync(this, @this => @this.Set(guid, @string, stringData, int32, uri, @dynamic)); }
+        else { Set(guid, @string, stringData, int32, uri, @dynamic); }
+
+        if (throwError) { throw new Exception(); }
+    }
+
+    protected virtual void Set(Guid guid, string @string, string stringData, int int32, Uri uri, object @dynamic)
     {
         Guid = guid;
         String = @string;
