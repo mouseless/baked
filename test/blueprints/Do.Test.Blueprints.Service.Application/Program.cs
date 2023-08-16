aa1088fd13bca68dd38524bce46161a55fcae423
Sefer Mirza <sefermirza@gmail.com>
2023-08-16 16:45:45 +0300

    review changes

--- test/blueprints/Do.Test.Blueprints.Service.Application/Program.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Application/Program.cs
@@ -1,7 +1,7 @@
 Forge.New
     .Service(
-            business: c => c.Default(),
-            database: c => c.MySql().ForDevelopment(c.Sqlite()),
-            configure: app => app.Features.AddConfigurationOverrider()
-        )
+        business: c => c.Default(),
+        database: c => c.MySql().ForDevelopment(c.Sqlite()),
+        configure: app => app.Features.AddConfigurationOverrider()
+    )
     .Run();
