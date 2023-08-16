9499eece43e82c908f46f772747d6c46b35bcaff
alptuncs <alptuncsever@gmail.com>
2023-08-16 21:47:05 +0300

    refactor exception building in ApplicationContext

--- src/core/Do.Architecture/Architecture/ApplicationContext.cs
+++ src/core/Do.Architecture/Architecture/ApplicationContext.cs
@@ -6,41 +6,26 @@ public class ApplicationContext
 
     public void Add<T>(T item) where T : notnull => _context[typeof(T)] = item;
 
+    public bool Has<T>() => _context.ContainsKey(typeof(T));
+
     public T Get<T>()
     {
         if (!Has<T>())
         {
-            throw BuildKeyNotFound<T>();
+            throw new KeyNotFoundException($"'{typeof(T).Name}' does not exist in context, " +
+                $"{BuildMessageDetails<T>()}"
+            );
         }
 
         return (T)_context[typeof(T)];
     }
 
-    public bool Has<T>() => _context.ContainsKey(typeof(T));
+    private string BuildMessageDetails<T>() =>
+        !_context.Any()
+            ? "because it is empty."
+            : GetAssignableTypes<T>().Any()
+                ? $"did you mean: {string.Join(", ", GetAssignableTypes<T>().Select(t => $"'{t.Name}'"))}?"
+                : $"available types are: {string.Join(", ", _context.Keys.Select(t => $"'{t.Name}'"))}.";
 
-    private KeyNotFoundException BuildKeyNotFound<T>()
-    {
-        var message = $"'{typeof(T).Name}' does not exist in context.";
-
-        if (_context.Count == 0)
-        {
-            message = message.Replace('.', ' ') + "because it is empty.";
-
-            return new(message);
-        }
-
-        var foundTypes = _context.Keys.Where(k => typeof(T).IsAssignableFrom(k)).ToList();
-        if (foundTypes.Any())
-        {
-            var typeNames = foundTypes.Select(f => $"'{f.Name}'").ToList();
-            message += " Did you mean: " + string.Join(", ", typeNames) + "?";
-
-            return new(message);
-        }
-
-        var types = _context.Keys.Select(k => $"'{k.Name}'").ToList();
-        message += " Available types are: " + string.Join(", ", types);
-
-        return new(message);
-    }
+    private IEnumerable<Type> GetAssignableTypes<T>() => _context.Keys.Where(t => t.IsAssignableTo(typeof(T)));
 }
