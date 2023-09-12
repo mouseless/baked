1910f0de6c5f11e6da8f56cf5801723c4ca3eecd
dncsvr <disever10@alm.ku.edu.tr>
2023-09-12 16:16:06 +0300

    add record not found exception handler

---
+++ src/blueprints/Do.Blueprints.Service/Orm/RecordNotFoundExceptionHandler.cs
@@ -0,0 +1,10 @@
+﻿using Do.ExceptionHandling;
+using System.Net;
+
+namespace Do.Orm;
+
+public class RecordNotFoundExceptionHandler : IExceptionHandler
+{
+    public bool CanHandle(Exception ex) => ex is RecordNotFoundException;
+    public ExceptionInfo Handle(Exception ex) => new((int)HttpStatusCode.NotFound, ex.Message);
+}
