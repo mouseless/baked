0255583f0143d6a84e60dd28ec59725f8db2fc03
Sefer Mirza <sefermirza@gmail.com>
2023-09-15 16:35:10 +0300

    minor changes

--- test/blueprints/Do.Test.Blueprints.Service.Application/RestApi/Analyzer/Demo.generated.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Application/RestApi/Analyzer/Demo.generated.cs
@@ -7,21 +7,6 @@ namespace Do.Test;
 [ApiController]
 public class DemoController
 {
-    [HttpPost]
-    [Produces("application/json")]
-    [Route("object-types")]
-    public object ObjectPost([FromQuery] object requestQuery) => requestQuery;
-
-    [HttpGet]
-    [Produces("application/json")]
-    [Route("object-types")]
-    public async Task<object> ObjectGet([FromRoute] object request)
-    {
-        await Task.Delay(10);
-
-        return request;
-    }
-
     [HttpPut]
     [Produces("application/json")]
     [Route("object-types")]
