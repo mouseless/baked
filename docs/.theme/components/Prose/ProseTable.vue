0664095526a606fe5b065f6d22387be1627ffe17
Sefer Mirza <sefermirza@gmail.com>
2023-09-28 16:05:30 +0300

    fix table issue

---
+++ docs/.theme/components/Prose/ProseTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="table-container">
+    <table>
+      <slot />
+    </table>
+  </div>
+</template>
+<script setup>
+import { onMounted } from "#imports";
+
+onMounted(() => {
+  const tableContainer = document.querySelector(".table-container");
+  const table = document.querySelector("table");
+
+  tableContainer.style.height = getComputedStyle(table).height;
+});
+</script>
+<style lang="scss" scoped>
+.table-container {
+  overflow-x: auto;
+  position: relative;
+  width: 100%;
+  table {
+    position: absolute;
+  }
+}
+</style>
