2027576165d672b8fbacda16ac039852d04d3dea
Sefer Mirza <sefermirza@gmail.com>
2023-11-22 10:42:30 +0300

    review change

--- src/blueprints/Do.Blueprints.Service.Application/Orm/Default/UserTypes/JsonObjectStringType.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Orm/Default/UserTypes/JsonObjectStringType.cs
@@ -5,8 +5,10 @@ namespace Do.Orm.Default.UserTypes;
 
 public class JsonObjectStringType : AbstractStringType
 {
+    const int ColumnLength = 64 * 1024;
+
     public JsonObjectStringType()
-        : base(new SqlType(System.Data.DbType.String, 64 * 1024)) { }
+        : base(new SqlType(System.Data.DbType.String, ColumnLength)) { }
 
     public override string Name => nameof(JsonObjectStringType);
 }
