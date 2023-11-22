d9c15b9b4a3e1dc455eee91a30a893cd3a542487
dncsvr <disever10@alm.ku.edu.tr>
2023-11-22 14:43:36 +0300

    add GetRequiredValue extension with default value option

---
+++ src/blueprints/Do.Blueprints.Service/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Do.Configuration;
+
+public static class ConfigurationExtensions
+{
+    public static T GetRequiredValue<T>(this IConfiguration configuration, string key,
+        T? defaultValue = default
+    ) => configuration.GetValue<T>(key) ?? defaultValue ??
+           throw new InvalidOperationException($"Looked for a value {key} in Configurations, but could not found");
+}
