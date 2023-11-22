86436ead9dbca15fa92a404bfebd5a8da4174c64
Sefer Mirza <sefermirza@gmail.com>
2023-11-22 16:16:43 +0300

    review changes

--- src/blueprints/Do.Blueprints.Service.Application/ExceptionHandling/Default/GenericADOExceptionHandler.cs
+++ src/blueprints/Do.Blueprints.Service.Application/ExceptionHandling/Default/GenericADOExceptionHandler.cs
@@ -6,5 +6,8 @@ public class GenericADOExceptionHandler : IExceptionHandler
 {
     public bool CanHandle(Exception ex) => ex is GenericADOException;
     public ExceptionInfo Handle(Exception ex) =>
-        new(500, ex.InnerException != null ? ex.InnerException.Message : ex.Message);
+        new(
+            (int)((HandledException)ex).StatusCode,
+            ex.InnerException != null ? ex.InnerException.Message : ex.Message
+        );
 }
