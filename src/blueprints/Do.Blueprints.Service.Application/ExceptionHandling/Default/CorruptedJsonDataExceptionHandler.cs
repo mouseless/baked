9c3cb722102fab0fd93d89ed6ce1c5e960fdbdbc
Sefer Mirza <sefermirza@gmail.com>
2023-11-23 10:48:52 +0300

    review & refactor

--- src/blueprints/Do.Blueprints.Service.Application/ExceptionHandling/Default/CorruptedJsonDataExceptionHandler.cs
+++ src/blueprints/Do.Blueprints.Service.Application/ExceptionHandling/Default/CorruptedJsonDataExceptionHandler.cs
@@ -1,13 +1,17 @@
+using System.Net;
 using NHibernate.Exceptions;
 
 namespace Do.ExceptionHandling.Default;
 
 public class CorruptedJsonDataExceptionHandler : IExceptionHandler
 {
-    public bool CanHandle(Exception ex) => ex is GenericADOException && ex.InnerException != null;
+    public bool CanHandle(Exception ex) =>
+        ex is GenericADOException &&
+        ex.InnerException is InvalidDataException;
+
     public ExceptionInfo Handle(Exception ex) =>
         new(
-            500,
+            (int)HttpStatusCode.InternalServerError,
             ex.InnerException?.Message ?? ex.Message
         );
 }
