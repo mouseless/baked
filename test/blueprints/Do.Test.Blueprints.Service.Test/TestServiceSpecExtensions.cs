abe94eda27d64a6286d91e248954f10e6fb3b3ed
alptuncs <alptuncsever@gmail.com>
2023-11-24 14:28:48 +0300

    add some test cases & helpers

---
+++ test/blueprints/Do.Test.Blueprints.Service.Test/TestServiceSpecExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Do.Testing;
+
+namespace Do.Test;
+
+public static class TestServiceSpecExtensions
+{
+    public static Entity AnEntity(this Stubber giveMe,
+       Guid? guid = default,
+       string? @string = default,
+       string? stringData = default,
+       int? int32 = default,
+       Uri? uri = default,
+       object? @dynamic = default,
+       Status? status = default
+    )
+    {
+        return giveMe.
+            A<Entity>().
+                With(
+                    guid ?? Guid.NewGuid(),
+                    @string ?? string.Empty,
+                    stringData ?? string.Empty,
+                    int32 ?? 0, uri ?? giveMe.AUrl(),
+                    dynamic ?? new { },
+                    status ?? Status.Disabled
+                );
+    }
+}
---
+++ test/blueprints/Do.Test.Blueprints.Service.Test/Transaction.cs
@@ -0,0 +1,37 @@
+﻿namespace Do.Test;
+
+public class Transaction : TestServiceSpec
+{
+    [Test]
+    public void Created_entity_persists()
+    {
+        var entity = GiveMe.AnEntity();
+
+        VerifyPersists(entity);
+    }
+
+    [Test]
+    public async Task Entity_update_does_not_rollback_when_done_asynchronously_if_an_error_occurs()
+    {
+        var entity = GiveMe.AnEntity(@string: "test");
+        var entities = GiveMe.The<Entities>();
+        var task = entity.Update(
+            guid: entity.Guid,
+            @string: "updated",
+            stringData: entity.StringData,
+            int32: entity.Int32,
+            uri: entity.Uri,
+            dynamic: entity.Dynamic,
+            status: entity.Status,
+            useTransaction: true,
+            throwError: true
+        );
+
+        await task.ShouldThrowAsync<Exception>();
+
+        var result = entities.By("updated").FirstOrDefault(e => e.Guid == entity.Guid);
+
+        result.ShouldNotBeNull();
+        result.String.ShouldBe(entity.String);
+    }
+}
