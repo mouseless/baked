e99255c8574cf6288da721e033a5fca9b5448194
Sefer Mirza <sefermirza@gmail.com>
2023-11-29 16:07:06 +0300

    review

--- src/blueprints/Do.Blueprints.Service.Application/ServiceSpecExtensions.cs
+++ src/blueprints/Do.Blueprints.Service.Application/ServiceSpecExtensions.cs
@@ -126,21 +126,22 @@ public static class ServiceSpecExtensions
 
     #region Settings
 
-    public static void ASetting(this Mocker mockMe,
-        string? key = default,
-        string? value = default
-    ) => mockMe.ASetting<string>(key: key ?? "Test:Configuration", value: value ?? "value");
-
     public static void ASetting<T>(this Mocker mockMe,
         string? key = default,
         T? value = default
+    ) => mockMe.ASetting<string>(key: key, value: $"{value}");
+
+    public static void ASetting(this Mocker mockMe,
+        string? key = default,
+        string? value = default
     )
     {
         key ??= "Test:Configuration";
+        value ??= "value";
 
         var spec = (ServiceSpec)mockMe.Spec;
 
-        spec.Settings[key] = $"{value}";
+        spec.Settings[key] = value;
     }
 
     internal static IConfiguration TheConfiguration(this Mocker mockMe,
