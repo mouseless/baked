5b1ec7e5b779029bc03b4290034f85c1be534913
dncsvr <disever10@alm.ku.edu.tr>
2023-12-11 15:56:38 +0300

    review changes
    - remove memory cache field and setup

--- test/blueprints/Do.Test.Blueprints.Service.Test/Caching/ScopedMemory.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Test/Caching/ScopedMemory.cs
@@ -4,30 +4,23 @@ namespace Do.Test.Caching;
 
 public class ScopedMemory : TestServiceSpec
 {
-    IMemoryCache _cache = default!;
-
-    public override void SetUp()
-    {
-        base.SetUp();
-
-        _cache = GiveMe.AMemoryCache();
-    }
-
     public override void TearDown()
     {
         base.TearDown();
 
-        _cache.VerifyCount(c => c is 0);
+        var cache = GiveMe.AMemoryCache();
+        cache.VerifyCount(c => c is 0);
     }
 
     [Test]
     public void Objects_can_be_cached_in_memory()
     {
-        var expected = _cache.GetOrCreate<object>("key", _ => new());
+        var cache = GiveMe.AMemoryCache();
+        var expected = GiveMe.AMemoryCache().GetOrCreate<object>("key", _ => new());
 
-        var actual = _cache.GetOrCreate<object>("key", _ => new());
+        var actual = cache.GetOrCreate<object>("key", _ => new());
 
         actual.ShouldBeSameAs(expected);
-        _cache.VerifyCount(c => c is 1);
+        cache.VerifyCount(c => c is 1);
     }
 }
