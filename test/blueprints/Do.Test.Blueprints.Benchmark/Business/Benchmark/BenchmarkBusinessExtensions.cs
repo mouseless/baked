65d0d624165c41af179f05eb638d6c236bd06bd3
dncsvr <disever10@alm.ku.edu.tr>
2023-12-22 15:07:21 +0300

    remove benchmark project from do

--- test/blueprints/Do.Test.Blueprints.Benchmark/Business/Benchmark/BenchmarkBusinessExtensions.cs
+++
@@ -1,9 +0,0 @@
-using Do.Business;
-using Do.Test.Benchmark;
-
-namespace Do;
-
-public static class BenchmarkBusinessExtensions
-{
-    public static BenchmarkBusinessFeature Benchmark(this BusinessConfigurator _) => new();
-}
--- test/blueprints/Do.Test.Blueprints.Benchmark/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-using Do.Test;
-
-BenchmarkRunner.Run<Build>();
