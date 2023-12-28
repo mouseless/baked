71dc6b9c5b8b545f44ceed64cc3401f5e89a1ee9
Sefer Mirza <sefermirza@gmail.com>
2023-12-28 16:34:48 +0300

    apply minor edits

--- src/blueprints/Do.Blueprints.Service.Application/Logging/Request/RequestLogMiddleware.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Logging/Request/RequestLogMiddleware.cs
@@ -13,22 +13,22 @@ public class RequestLogMiddleware(ILogger<RequestLogMiddleware> _logger, Request
         try
         {
             await _next(context);
+
+            var exception = context.Features.Get<IExceptionHandlerFeature>();
+            if (exception?.Error is not null)
+            {
+                _logger.LogError(exception: exception.Error, message: exception.Error.Message);
+            }
+            else
+            {
+                _logger.LogInformation(message: $"End: {context.Request.Path} StatusCode: {context.Response.StatusCode}");
+            }
         }
         catch (Exception e)
         {
             _logger.LogError(exception: e, message: e.Message);
 
             throw;
-        }
-
-        var exception = context.Features.Get<IExceptionHandlerFeature>();
-        if (exception?.Error is not null)
-        {
-            _logger.LogError(exception: exception.Error, message: exception.Error.Message);
-        }
-        else
-        {
-            _logger.LogInformation(message: $"End: {context.Request.Path} StatusCode: {context.Response.StatusCode}");
         }
     }
 }
