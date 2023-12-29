cd17223c7d12c3ed425527570b1f04a2deb3b4fc
Sefer Mirza <sefermirza@gmail.com>
2023-12-29 14:42:26 +0300

    remove test code

--- test/blueprints/Do.Test.Blueprints.Service.Application/Program.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Application/Program.cs
@@ -2,7 +2,6 @@ Forge.New
     .Service(
         business: c => c.Default(),
         database: c => c.MySql().ForDevelopment(c.Sqlite()),
-        configure: app => app.Features.AddConfigurationOverrider(),
-        exceptionHandling: e => e.Default(exceptionTypeUrl: "http://www.google.com/{0}")
+        configure: app => app.Features.AddConfigurationOverrider()
     )
     .Run();
