bf3392c5c961695dfd5dcd0aa556c9d834e2c45d
alptuncs <alptuncsever@gmail.com>
2024-01-03 23:31:55 +0300

    DoBanner case and implementation

--- test/core/Do.Test.Architecture/Architecture/Branding/PrintingBanner.cs
+++ test/core/Do.Test.Architecture/Architecture/Branding/PrintingBanner.cs
@@ -1,4 +1,6 @@
-﻿namespace Do.Test.Architecture.Branding;
+﻿using Do.Branding;
+
+namespace Do.Test.Architecture.Branding;
 
 public class PrintingBanner : ArchitectureSpec
 {
@@ -11,5 +13,25 @@ public class PrintingBanner : ArchitectureSpec
         forge.Application(_ => { });
 
         banner.VerifyPrinted();
+    }
+
+    [Test]
+    public void Prints_the_correct_banner()
+    {
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var banner = GiveMe.ADoBanner();
+
+        banner.Print();
+
+        var outputLines = writer.ToString().Split(Environment.NewLine);
+
+        outputLines[0].ShouldBe("----------------------------------------------");
+        outputLines[1].ShouldBe(string.Empty);
+        outputLines[2].ShouldBe("  ██        ████████    ██████████");
+        outputLines[3].ShouldBe("    ██      ██      ██  ██      ██");
+        outputLines[4].ShouldBe("      ██    ██      ██  ██      ██");
+        outputLines[5].ShouldBe("    ██      ██      ██  ██      ██");
+        outputLines[6].ShouldBe("  ██        ████████    ██████████  ██████████");
     }
 }
--- test/core/Do.Test.Architecture/ArchitectureSpecExtensions.cs
+++ test/core/Do.Test.Architecture/ArchitectureSpecExtensions.cs
@@ -92,6 +92,9 @@ public static class ArchitectureSpecExtensions
     public static void VerifyPrinted(this IBanner source) =>
         Mock.Get(source).Verify(b => b.Print());
 
+    public static DoBanner ADoBanner(this Stubber _) =>
+        new();
+
     #endregion
 
     #region Layer
