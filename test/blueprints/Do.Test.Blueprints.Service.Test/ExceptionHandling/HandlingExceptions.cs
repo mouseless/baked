e963ee1bd86c43a23b2de28c19cdac2a1e5ba48d
alptuncs <alptuncsever@gmail.com>
2024-01-04 23:35:47 +0300

    add exception handling test cases, add some IQueryContext test cases

--- test/blueprints/Do.Test.Blueprints.Service.Test/ExceptionHandling/HandlingExceptions.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Test/ExceptionHandling/HandlingExceptions.cs
@@ -1,4 +1,9 @@
-﻿namespace Do.Test.ExceptionHandling;
+﻿using Do.ExceptionHandling;
+using Do.ExceptionHandling.Default;
+using Do.Orm;
+using System.Net;
+
+namespace Do.Test.ExceptionHandling;
 
 public class HandlingExceptions : TestServiceSpec
 {
@@ -10,5 +15,50 @@ public class HandlingExceptions : TestServiceSpec
         var task = () => singleton.TestException(handled: true);
 
         task.ShouldThrow<TestServiceHandledException>();
+    }
+
+    [Test]
+    public void HandledExceptionHandler_transforms_exception_to_ExceptionInfo()
+    {
+        var exception = new TestServiceHandledException("message");
+        var handler = new HandledExceptionHandler();
+
+        var actual = handler.Handle(exception);
+
+        actual.ShouldBeOfType<ExceptionInfo>();
+        actual.Exception.ShouldBe(exception);
+        actual.Code.ShouldBe((int)HttpStatusCode.BadRequest);
+        actual.Body.ShouldBe("message");
+        actual.ExtraData.ShouldBeEmpty();
+    }
+
+    [Test]
+    public void RecordNotFoundException_for_calls_primary_constructor_with_correct_mapping()
+    {
+        var recordNotFoundException = RecordNotFoundException.For<Entity>(field: "Id",
+            value: GiveMe.AGuid("b4b6bd2b-b8f3-414e-bd84-1e044cbe77dc")
+        );
+
+        recordNotFoundException.Message.ShouldBe($"Entity with Id: 'b4b6bd2b-b8f3-414e-bd84-1e044cbe77dc' does not exist");
+    }
+
+    [Test]
+    public void Query_throws_RecordNotFoundException_when_entity_with_given_id_is_not_found()
+    {
+        var entityQueryContext = GiveMe.The<IQueryContext<Entity>>();
+        var task = () => entityQueryContext.SingleById(GiveMe.AGuid());
+
+        task.ShouldThrow<RecordNotFoundException>();
+    }
+
+    [Test]
+    public void RecordNotFoundException_status_code_is_not_found()
+    {
+        var entityQueryContext = GiveMe.The<IQueryContext<Entity>>();
+        var task = () => entityQueryContext.SingleById(GiveMe.AGuid());
+
+        var actual = task.ShouldThrow<RecordNotFoundException>();
+
+        actual.StatusCode.ShouldBe(HttpStatusCode.NotFound);
     }
 }
---
+++ test/blueprints/Do.Test.Blueprints.Service.Test/Orm/QueryingEntities.cs
@@ -0,0 +1,18 @@
+﻿using Do.Orm;
+
+namespace Do.Test.Orm;
+
+public class QueryingEntities : TestServiceSpec
+{
+    [Test]
+    public void Returns_a_single_entity_by_the_given_condition()
+    {
+        var guid = GiveMe.AGuid("b4b6bd2b-b8f3-414e-bd84-1e044cbe77dc");
+        var entity = GiveMe.AnEntity(guid: guid);
+        var entitites = GiveMe.The<IQueryContext<Entity>>();
+
+        var actual = entitites.SingleBy(e => e.Guid == guid);
+
+        actual.ShouldBeEquivalentTo(entity);
+    }
+}
