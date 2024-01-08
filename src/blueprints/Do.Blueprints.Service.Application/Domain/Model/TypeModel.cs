87c5780b29226d944d18f889932376c74f5828c3
dncsvr <disever10@alm.ku.edu.tr>
2024-01-08 19:26:11 +0300

    minor format edit

--- src/blueprints/Do.Blueprints.Service.Application/Domain/Model/TypeModel.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Domain/Model/TypeModel.cs
@@ -11,8 +11,7 @@ public record TypeModel(
     public TypeModel(Type type)
         : this(type, type.Name, [])
     {
-        var methodInfos = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly) ??
-            [];
+        var methodInfos = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly) ?? [];
 
         foreach (var method in methodInfos)
         {
