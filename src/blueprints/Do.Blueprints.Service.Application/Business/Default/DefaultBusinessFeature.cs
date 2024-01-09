0bede5129058a1922170e49fc335f49477068ed7
dncsvr <disever10@alm.ku.edu.tr>
2024-01-09 17:02:50 +0300

    revert `DefaultBusinessFeature` implementation

--- src/blueprints/Do.Blueprints.Service.Application/Business/Default/DefaultBusinessFeature.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Business/Default/DefaultBusinessFeature.cs
@@ -1,4 +1,5 @@
 ﻿using Do.Architecture;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Do.Business.Default;
 
@@ -6,5 +7,20 @@ public class DefaultBusinessFeature : IFeature<BusinessConfigurator>
 {
     public void Configure(LayerConfigurator configurator)
     {
+        configurator.ConfigureServiceCollection(services =>
+        {
+            var domainModel = configurator.Context.GetDomainModel();
+            foreach (var model in domainModel.Types)
+            {
+                if (model.Methods.Any(m => m.Name.Equals("With") && m.ReturnType.Equals(model.Type)))
+                {
+                    services.AddTransientWithFactory(model.Type);
+                }
+                else
+                {
+                    services.AddSingleton(model.Type);
+                }
+            }
+        });
     }
 }
