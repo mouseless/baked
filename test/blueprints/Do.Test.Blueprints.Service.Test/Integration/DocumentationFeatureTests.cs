888d48272f738e0064d7bfd9d874ef70faa1eb03
alptuncs <alptuncsever@gmail.com>
2024-01-11 12:10:07 +0300

    add integration test spec & demo documentation feature fixture

---
+++ test/blueprints/Do.Test.Blueprints.Service.Test/Integration/DocumentationFeatureTests.cs
@@ -0,0 +1,18 @@
+﻿using Do.Architecture;
+
+namespace Do.Test.Integration;
+
+public class DocumentationFeatureTests : IntegrationSpec
+{
+    [Test]
+    public async Task Application_root_is_swagger_index_page()
+    {
+        var client = Factory.CreateClient();
+
+        var response = await client.GetAsync("/");
+
+        response.EnsureSuccessStatusCode();
+        response.RequestMessage.ShouldNotBeNull();
+        response.RequestMessage.RequestUri.ShouldBe("http://localhost/swagger/index.html");
+    }
+}
---
+++ test/blueprints/Do.Test.Blueprints.Service.Test/IntegrationSpec.cs
@@ -0,0 +1,36 @@
+﻿using Do.Architecture;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Do.Test;
+
+public abstract class IntegrationSpec
+{
+    static WebApplicationFactory<TestProgram> _factory = default!;
+
+    static IntegrationSpec()
+    {
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+
+        _factory = new WebApplicationFactory<TestProgram>();
+    }
+
+    public static void Run() =>
+        Forge.New
+            .Service(
+               business: c => c.Default(),
+               database: c => c.Sqlite(),
+               exceptionHandling: ex => ex.Default(typeUrlFormat: "https://do.mouseless.codes/errors/{0}"),
+               configure: app => app.Features.AddConfigurationOverrider()
+           )
+        .Run();
+
+    public WebApplicationFactory<TestProgram> Factory => _factory;
+}
+
+public class TestProgram
+{
+    public static void Main(string[] args)
+    {
+        IntegrationSpec.Run();
+    }
+}
