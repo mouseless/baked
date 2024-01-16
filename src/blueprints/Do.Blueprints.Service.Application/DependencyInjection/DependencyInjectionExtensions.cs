8aa09bf91d072094d84ca98fba57a63d7aab321d
dncsvr <disever10@alm.ku.edu.tr>
2024-01-16 13:48:34 +0300

    revert changes in `DependencyInjectionExtensions`

--- src/blueprints/Do.Blueprints.Service.Application/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/blueprints/Do.Blueprints.Service.Application/DependencyInjection/DependencyInjectionExtensions.cs
@@ -7,7 +7,6 @@ namespace Do;
 public static class DependencyInjectionExtensions
 {
     public static void AddDependencyInjection(this List<ILayer> source) => source.Add(new DependencyInjectionLayer());
-    public static void ConfigureServiceCollection(this LayerConfigurator source, Action<IServiceCollection> configuration) => source.Configure(configuration);
-
     public static IServiceCollection GetServiceCollection(this ApplicationContext source) => source.Get<IServiceCollection>();
+    public static void ConfigureServiceCollection(this LayerConfigurator source, Action<IServiceCollection> configuration) => source.Configure(configuration);
 }
