25feb779a9ee892004044d6a0e14866b5d56bf40
alptuncs <alptuncsever@gmail.com>
2024-01-16 17:11:47 +0300

    review, print banner

--- test/core/Do.Test.Architecture/Architecture/Branding/PrintingBanner.cs
+++ test/core/Do.Test.Architecture/Architecture/Branding/PrintingBanner.cs
@@ -1,9 +1,33 @@
 ﻿using Do.Branding;
+using System.Diagnostics;
 
 namespace Do.Test.Architecture.Branding;
 
 public class PrintingBanner : ArchitectureSpec
 {
+    TextWriter _realOut = default!;
+    TextWriter _fakeOut = default!;
+
+    public override void SetUp()
+    {
+        base.SetUp();
+
+        _realOut = Console.Out;
+        Console.SetOut(_fakeOut = new StringWriter());
+    }
+
+    public override void TearDown()
+    {
+        base.TearDown();
+
+        Console.SetOut(_realOut);
+    }
+
+    string ConsoleOutput => _fakeOut?.ToString() ?? string.Empty;
+
+    // Version is shortened to Ver to keep banner width fixed
+    string Ver => FileVersionInfo.GetVersionInfo(typeof(IBanner).Assembly.Location).FileVersion?[..^2] ?? throw new("Version not found");
+
     [Test]
     public void It_prints_banner_prior_to_build()
     {
@@ -16,22 +40,27 @@ public class PrintingBanner : ArchitectureSpec
     }
 
     [Test]
-    public void Prints_the_correct_banner()
+    public void Do_banner_prints_do_logo__version_and_project_links_to_console()
     {
-        var writer = new StringWriter();
-        Console.SetOut(writer);
         var banner = GiveMe.ADoBanner();
 
         banner.Print();
 
-        var output = string.Join(Environment.NewLine, writer.ToString().Split(Environment.NewLine)[0..7]);
-
-        output.ShouldBe(@"----------------------------------------------
+        ConsoleOutput.ShouldMatch($@"
+----------------------------------------------
 
   ██        ████████    ██████████
     ██      ██      ██  ██      ██
       ██    ██      ██  ██      ██
     ██      ██      ██  ██      ██
-  ██        ████████    ██████████  ██████████");
+  ██        ████████    ██████████  ██████████
+
+------------------- v{Ver} -------------------
+
+Docs: https://do.mouseless.codes
+Source: https://github.com/mouseless/do
+
+----------------------------------------------
+".Trim());
     }
 }
