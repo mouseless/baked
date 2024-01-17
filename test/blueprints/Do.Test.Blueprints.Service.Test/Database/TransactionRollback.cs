1fb4b3796d1c273c5bb3c657c942dfbcf01e5f30
alptuncs <alptuncsever@gmail.com>
2024-01-17 16:02:47 +0300

    review, move tests to fixtures, remove extensions

---
+++ test/blueprints/Do.Test.Blueprints.Service.Test/Database/TransactionRollback.cs
@@ -0,0 +1,28 @@
+﻿using Do.Architecture;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace Do.Test.Database;
+
+public class FlatTransactionalWork : TestServiceNfr
+{
+    protected override Application ForgeApplication() =>
+        Forge.New
+            .Service(
+                business: c => c.Default(),
+                database: c => c.Sqlite("Do.Test.Blueprits.Service.Test")
+            );
+
+    [Test]
+    public async Task Entity_created_by_a_transaction_committed_asynchronously_persists_when_an_error_occurs()
+    {
+        await Client.PostAsync($"singleton/test-transaction-action", null);
+
+        var entitiesContent = await Client.GetAsync("entities");
+        var result = await entitiesContent.Content.ReadFromJsonAsync<List<JsonElement>>() ?? throw new("No entities in database");
+
+        result.ShouldNotBeNull();
+        var stringData = $"{result.Last().GetProperty("stringData")}";
+        stringData.ShouldBe("transaction action");
+    }
+}
