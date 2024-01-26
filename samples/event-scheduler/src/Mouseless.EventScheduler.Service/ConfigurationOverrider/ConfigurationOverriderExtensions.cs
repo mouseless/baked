e7498c64d0d2c2a4f5b2e25a7af21d22fa06560c
dncsvr <disever10@alm.ku.edu.tr>
2024-01-26 10:06:12 +0300

    edit EventScheduler business feature setup
    - remove ConfigurationOverrider feature
    - ignore SA1010 rule

--- samples/event-scheduler/src/Mouseless.EventScheduler.Service/ConfigurationOverrider/ConfigurationOverriderExtensions.cs
+++
@@ -1,8 +0,0 @@
-using Do.Architecture;
-
-namespace Do;
-
-public static class ConfigurationOverriderExtensions
-{
-    public static void AddConfigurationOverrider(this List<IFeature> source) => source.Add(new ConfigurationOverriderFeature());
-}
