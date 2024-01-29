74d28d801f15af9155b70da83ff96c1d03114502
dncsvr <disever10@alm.ku.edu.tr>
2024-01-29 14:58:34 +0300

    rename test case

--- test/blueprints/Do.Test.Blueprints.Service.Test/Business/RegisteringServices.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Test/Business/RegisteringServices.cs
@@ -162,7 +162,7 @@ public class RegisteringServices : TestServiceSpec
     }
 
     [Test]
-    public void Types_having_no_with_method_but_public_properties_are_not_registered_as_singleton([Values(typeof(ClassDTO))] Type type)
+    public void Types_having_no_with_method_but_public_properties_are_not_registered([Values(typeof(ClassDTO))] Type type)
     {
         var action = () => GiveMe.The(type);
 
