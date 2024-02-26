1c2a434c160f4f0130689bd427c9d65decaef956
dncsvr <disever10@alm.ku.edu.tr>
2024-02-26 16:10:17 +0300

    edit handling unauthorized exception test case
    - rename test fixture to `GeneratingUnauthorizedExceptionResponse`

---
+++ test/blueprints/Do.Test.Blueprints.Service.Test/ExceptionHandling/GeneratingUnauthorizedAccessResponse.cs
@@ -0,0 +1,30 @@
+﻿using Do.Architecture;
+using Do.Authentication;
+using Do.ExceptionHandling;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Do.Test.ExceptionHandling;
+
+public class GeneratingUnauthorizedAccessResponse : TestServiceNfr
+{
+    protected override Func<AuthenticationConfigurator, IFeature<AuthenticationConfigurator>>? Authentication =>
+        c => c.FixedToken(["Backend"]);
+    protected override Func<ExceptionHandlingConfigurator, IFeature<ExceptionHandlingConfigurator>>? ExceptionHandling =>
+       c => c.Default(typeUrlFormat: "https://do.mouseless.codes/errors/{0}");
+
+    [Test]
+    public async Task Unauthorized_access_exceptions_are_handled_with_its_own_handler()
+    {
+        var response = await Client.GetAsync("singleton/time");
+
+        var problemDetails = response.Content.ReadFromJsonAsync<ProblemDetails>().Result;
+
+        problemDetails.ShouldNotBeNull();
+        problemDetails.Detail.ShouldBe("Attempted to perform an unauthorized operation.");
+        problemDetails.Status.ShouldBe((int)HttpStatusCode.Unauthorized);
+        problemDetails.Title.ShouldBe("Unauthorized Access");
+        problemDetails.Type.ShouldBe("https://do.mouseless.codes/errors/unauthorized-access");
+    }
+}
