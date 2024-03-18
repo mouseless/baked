e0a475819385e0cb5ed4f6d696aa7fe733a00f90
dncsvr <disever10@alm.ku.edu.tr>
2024-03-18 17:31:43 +0300

    rename collection names to `_assemblies` and `_types`

--- src/blueprints/Do.Blueprints.Service.Application/Domain/DomainLayer.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Domain/DomainLayer.cs
@@ -8,30 +8,30 @@ namespace Do.Domain;
 
 public class DomainLayer : LayerBase<BuildConfiguration>
 {
-    readonly IDomainAssemblyCollection _domainAssemblyCollection = new DomainAssemblyCollection();
-    readonly IDomainTypeCollection _domainTypeCollection = new DomainTypeCollection();
+    readonly IDomainAssemblyCollection _assemblies = new DomainAssemblyCollection();
+    readonly IDomainTypeCollection _types = new DomainTypeCollection();
     readonly DomainBuilderOptions _domainBuilderOptions = new();
 
     protected override PhaseContext GetContext(BuildConfiguration phase) =>
         phase.CreateContextBuilder()
-            .Add<IDomainAssemblyCollection>(_domainAssemblyCollection)
-            .Add<IDomainTypeCollection>(_domainTypeCollection)
+            .Add<IDomainAssemblyCollection>(_assemblies)
+            .Add<IDomainTypeCollection>(_types)
             .Add<DomainBuilderOptions>(_domainBuilderOptions)
             .Build();
 
     protected override IEnumerable<IPhase> GetPhases()
     {
-        yield return new BuildDomain(_domainAssemblyCollection, _domainTypeCollection, _domainBuilderOptions);
+        yield return new BuildDomain(_assemblies, _types, _domainBuilderOptions);
     }
 
-    public class BuildDomain(IDomainAssemblyCollection _domainAssemblyCollection, IDomainTypeCollection _domainTypeCollection, DomainBuilderOptions _domainBuilderOptions)
+    public class BuildDomain(IDomainAssemblyCollection _assemblies, IDomainTypeCollection _types, DomainBuilderOptions _domainBuilderOptions)
         : PhaseBase<ConfigurationManager>(PhaseOrder.Early)
     {
         protected override void Initialize(ConfigurationManager _)
         {
             var builder = new DomainModelBuilder(_domainBuilderOptions);
 
-            var model = builder.BuildFrom(_domainAssemblyCollection, _domainTypeCollection);
+            var model = builder.BuildFrom(_assemblies, _types);
 
             Context.Add<DomainModel>(model);
         }
