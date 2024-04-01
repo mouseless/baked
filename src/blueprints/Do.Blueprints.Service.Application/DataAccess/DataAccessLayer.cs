f48fc6c830e9589e0fb949a5536aca8600ed1bc0
Sefer Mirza <sefermirza@gmail.com>
2024-04-01 17:00:25 +0300

    undo expression body for constructor

--- src/blueprints/Do.Blueprints.Service.Application/DataAccess/DataAccessLayer.cs
+++ src/blueprints/Do.Blueprints.Service.Application/DataAccess/DataAccessLayer.cs
@@ -20,8 +20,10 @@ public class DataAccessLayer : LayerBase<AddServices>
     volatile bool _exported = false;
     readonly object _exportedLock = new();
 
-    public DataAccessLayer() =>
+    public DataAccessLayer()
+    {
         _autoPersistenceModel = new(new DelegatedAutomappingConfiguration(_automappingConfiguration));
+    }
 
     protected override PhaseContext GetContext(AddServices phase)
     {
--- src/blueprints/Do.Blueprints.Service.Application/Database/MySql/CustomMySQL57Dialect.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Database/MySql/CustomMySQL57Dialect.cs
@@ -5,6 +5,8 @@ namespace Do.Database.MySql;
 
 public class CustomMySQL57Dialect : MySQL57Dialect
 {
-    public CustomMySQL57Dialect() =>
+    public CustomMySQL57Dialect()
+    {
         RegisterColumnType(DbType.String, 1023, "VARCHAR($l)");
+    }
 }
