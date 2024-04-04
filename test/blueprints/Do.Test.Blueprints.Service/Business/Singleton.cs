67ac82ad289243d7992b30501e39ed9916b9b071
dncsvr <disever10@alm.ku.edu.tr>
2024-04-04 15:22:17 +0300

    move service registration and api generation test classes to Business folder
    - move building non domain types test fixture to business test folder

---
+++ test/blueprints/Do.Test.Blueprints.Service/Business/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using Do.Communication;
+using Do.Database;
+using Newtonsoft.Json;
+
+namespace Do.Test;
+
+public class Singleton(
+    TimeProvider _timeProvider,
+    Func<OperationWithGenericParameter<Entity>> _newOperationWithGenericParameter
+) : SingletonBase(_timeProvider), IInterface
+{
+    public string TestOperationWithGenericParameter(string parameter)
+    {
+        return _newOperationWithGenericParameter()
+            .With(parameter)
+            .Execute();
+    }
+}
