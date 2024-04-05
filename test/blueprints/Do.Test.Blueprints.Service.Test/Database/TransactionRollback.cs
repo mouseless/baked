001d2cfc1b9a23f902376f7375950299336bb0ee
dncsvr <disever10@alm.ku.edu.tr>
2024-04-05 16:08:07 +0300

    add status code should not be not found assertion to transaction tests

--- test/blueprints/Do.Test.Blueprints.Service.Test/Database/TransactionRollback.cs
+++ test/blueprints/Do.Test.Blueprints.Service.Test/Database/TransactionRollback.cs
@@ -15,33 +15,36 @@ public class TransactionRollback : TestServiceNfr
     {
         var @string = $"{Guid.NewGuid()}";
         var content = JsonContent.Create(new { @string });
-        await Client.PostAsync($"transaction/rollback", content);
+        var response = await Client.PostAsync($"transaction/rollback", content);
 
         var entitiesContent = await Client.GetAsync("entities");
         dynamic? result = await entitiesContent.Content.Deserialize();
 
+        response.StatusCode.ShouldNotBe(System.Net.HttpStatusCode.NotFound);
         ((string?)result?.Last.String)?.ShouldNotBe($"{@string}");
     }
 
     [Test]
     public async Task Entity_created_by_a_transaction_committed_asynchronously_persists_when_an_error_occurs()
     {
-        await Client.PostAsync($"transaction/commit-action", null);
+        var response = await Client.PostAsync($"transaction/commit-action", null);
 
         var entitiesContent = await Client.GetAsync("entities");
         dynamic? result = await entitiesContent.Content.Deserialize();
 
+        response.StatusCode.ShouldNotBe(System.Net.HttpStatusCode.NotFound);
         ((string?)result?.Last.String)?.ShouldBe("transaction action");
     }
 
     [Test]
     public async Task Only_the_updates_outside_of_transaction_are_rolled_back_when_an_error_occurs()
     {
-        await Client.PostAsync($"transaction/commit-func", null);
+        var response = await Client.PostAsync($"transaction/commit-func", null);
 
         var entitiesContent = await Client.GetAsync("entities");
         dynamic? result = await entitiesContent.Content.Deserialize();
 
+        response.StatusCode.ShouldNotBe(System.Net.HttpStatusCode.NotFound);
         ((string?)result?.Last.String)?.ShouldBe("transaction func");
     }
 }
