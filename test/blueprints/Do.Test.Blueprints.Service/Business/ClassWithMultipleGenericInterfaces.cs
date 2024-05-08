c39b52157c87fe8f88fbb7aacdd0680e939e1fd3
dncsvr <disever10@alm.ku.edu.tr>
2024-05-08 17:59:10 +0300

    fix multiple generic interface bug

---
+++ test/blueprints/Do.Test.Blueprints.Service/Business/ClassWithMultipleGenericInterfaces.cs
@@ -0,0 +1,12 @@
+﻿namespace Do.Test.Business;
+
+public class ClassWithMultipleGenericInterfaces : IEquatable<int>, IEquatable<string>
+{
+    public int Numeric { get; set; } = default!;
+
+    public bool Equals(int other) =>
+        Numeric.Equals(other);
+
+    public bool Equals(string? other) =>
+        $"{Numeric}".Equals(other);
+}
