9848dd09d1d459a352127e36a847626521ea4483
dncsvr <disever10@alm.ku.edu.tr>
2024-05-13 20:56:39 +0300

    remove helpers from authentication extensions

--- src/blueprints/Do.Blueprints.Service.Application/Authentication/AuthenticationExtensions.cs
+++ src/blueprints/Do.Blueprints.Service.Application/Authentication/AuthenticationExtensions.cs
@@ -1,12 +1,10 @@
 ﻿using Do.Architecture;
 using Do.Authentication;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Security.Claims;
 
 namespace Do;
 
@@ -30,35 +28,4 @@ public static class AuthenticationExtensions
 
         return metadata?.GetMetadata<T>() is not null;
     }
-
-    public static bool ShouldBeAuthenticatedResult(this AuthenticateResult result,
-        IEnumerable<Claim>? claims = default,
-        bool ensureClaimValue = false
-    )
-    {
-        if (result.Failure is not null) { return false; }
-        if (result.Principal is null) { return false; }
-        if (!result.Succeeded) { return false; }
-
-        if (claims is not null)
-        {
-            foreach (var claim in claims)
-            {
-                if (
-                    result.Principal.Claims.FirstOrDefault(c =>
-                        c.Type == claim.Type &&
-                        (!ensureClaimValue || c.Value == claim.Value)
-                    ) is null
-                )
-                {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    public static bool ShouldBeFailedResult(this AuthenticateResult result) =>
-        result.Failure is not null && !result.Succeeded;
 }
