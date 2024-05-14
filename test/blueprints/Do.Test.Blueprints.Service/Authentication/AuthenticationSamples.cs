45076c6526a3c455f3e30477c0072261b645f42f
dncsvr <disever10@alm.ku.edu.tr>
2024-05-14 11:10:04 +0300

    log form post authenticate parameter value to fix unused parameter error

--- test/blueprints/Do.Test.Blueprints.Service/Authentication/AuthenticationSamples.cs
+++ test/blueprints/Do.Test.Blueprints.Service/Authentication/AuthenticationSamples.cs
@@ -1,12 +1,20 @@
-﻿using System.Security.Claims;
+﻿using Microsoft.Extensions.Logging;
+using System.Security.Claims;
 
 namespace Do.Test.Authentication;
 
-public class AuthenticationSamples(Func<ClaimsPrincipal> _getClaims)
+public class AuthenticationSamples(
+    Func<ClaimsPrincipal> _getClaims,
+    ILogger<AuthenticationSamples> _logger
+)
 {
     public string? Authenticate() =>
         _getClaims().Identity?.AuthenticationType;
 
-    public string? FormPostAuthenticate(object value) =>
-        _getClaims().Identity?.AuthenticationType;
+    public string? FormPostAuthenticate(object value)
+    {
+        _logger.LogInformation($"Form post authenticate is called with value:'{value}'");
+
+        return _getClaims().Identity?.AuthenticationType;
+    }
 }
