cd545b324ad072d402314433a52910ad19772c5f
dncsvr <disever10@alm.ku.edu.tr>
2024-10-10 15:03:57 +0300

    update samples project
    - add entity extensions sample
    - add configuration overrider and override route parts convention

---
+++ samples/event-scheduler/src/Mouseless.EventScheduler/DeleteMeetingContact.cs
@@ -0,0 +1,32 @@
+﻿using Baked.Business;
+
+namespace Mouseless.EventScheduler;
+
+public class DeleteMeetingContact(MeetingContacts _meetingContacts)
+{
+    Meeting _meeting = default!;
+
+    internal DeleteMeetingContact With(Meeting meeting)
+    {
+        _meeting = meeting;
+
+        return this;
+    }
+
+    public void Execute(Contact contact)
+    {
+        var result = _meetingContacts.SingleBy(_meeting, contact);
+
+        result?.Delete();
+    }
+
+    public static implicit operator DeleteMeetingContact(Meeting meeting) =>
+            meeting.Cast().To<DeleteMeetingContact>();
+}
+
+public class DeleteMeetingContacts(Func<DeleteMeetingContact> _newEntityExtension)
+    : ICasts<Meeting, DeleteMeetingContact>
+{
+    DeleteMeetingContact ICasts<Meeting, DeleteMeetingContact>.To(Meeting from) =>
+        _newEntityExtension().With(from);
+}
