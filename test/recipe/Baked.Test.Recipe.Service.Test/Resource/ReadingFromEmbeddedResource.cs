a09f7df65f8578c3e5d609307430c7c8ecd1d314
dncsvr <disever10@alm.ku.edu.tr>
2024-10-14 15:40:08 +0300

    add reading from embedded resource spec !!! TESTS FAILING !!!

---
+++ test/recipe/Baked.Test.Recipe.Service.Test/Resource/ReadingFromEmbeddedResource.cs
@@ -0,0 +1,7 @@
+﻿namespace Baked.Test.Resource;
+
+class ReadingFromEmbeddedResource : TestServiceSpec
+{
+    [Test]
+    public void Contents_of_an_embedded_resource_can_be_read() => this.ShouldFail();
+}
