5d17a498f13accece603d96b461e76f0f8a7f4fa
dncsvr <disever10@alm.ku.edu.tr>
2024-10-21 16:42:44 +0300

    rename `CreateReadStream` to `CreateStreamReader`

--- src/recipe/Baked.Recipe.Service/Runtime/FileProviderExtensions.cs
+++ src/recipe/Baked.Recipe.Service/Runtime/FileProviderExtensions.cs
@@ -6,23 +6,23 @@ public static class FileProviderExtensions
 {
     public static string? ReadAsString(this IFileProvider provider, string subPath)
     {
-        using var streamReader = provider.CreateReadStream(subPath);
+        using var streamReader = provider.CreateStreamReader(subPath);
 
         return streamReader.ReadToEnd();
     }
 
     public static async Task<string?> ReadAsStringAsync(this IFileProvider provider, string subPath)
     {
-        using var streamReader = provider.CreateReadStream(subPath);
+        using var streamReader = provider.CreateStreamReader(subPath);
 
         return await streamReader.ReadToEndAsync();
     }
 
-    static StreamReader CreateReadStream(this IFileProvider provider, string subPath)
+    static StreamReader CreateStreamReader(this IFileProvider provider, string subPath)
     {
         var fileInfo = provider.GetFileInfo(subPath);
         if (!fileInfo.Exists) { return StreamReader.Null; }
 
-        return new StreamReader(fileInfo.CreateReadStream(), Encoding.UTF8);
+        return new(fileInfo.CreateReadStream(), Encoding.UTF8);
     }
 }
