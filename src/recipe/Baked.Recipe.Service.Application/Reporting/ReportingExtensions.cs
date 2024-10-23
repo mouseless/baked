b7fdc40ba98ab8708796ed749cc1f14da2386338
cihandeniz <cihan@gcbrains.com>
2024-10-23 17:08:17 +0300

    implement mock reporting feature

--- src/recipe/Baked.Recipe.Service.Application/Reporting/ReportingExtensions.cs
+++ src/recipe/Baked.Recipe.Service.Application/Reporting/ReportingExtensions.cs
@@ -1,5 +1,7 @@
 using Baked.Architecture;
 using Baked.Reporting;
+using Baked.Testing;
+using Moq;
 
 namespace Baked;
 
@@ -7,4 +9,40 @@ public static class ReportingExtensions
 {
     public static void AddReporting(this List<IFeature> features, Func<ReportingConfigurator, IFeature<ReportingConfigurator>> configure) =>
         features.Add(configure(new()));
+
+    public static IReportContext TheReportContext(this Mocker mockMe,
+        object?[][]? data = default
+    )
+    {
+        data ??= [];
+
+        var result = Mock.Get(mockMe.Spec.GiveMe.The<IReportContext>());
+
+        if (data is not null)
+        {
+            result
+                .Setup(df => df.Execute(It.IsAny<string>(), It.IsAny<Dictionary<string, object>>()))
+                .ReturnsAsync(data);
+        }
+
+        return result.Object;
+    }
+
+    public static void VerifyExecute(this IReportContext dataFetcher,
+        string? queryName = default,
+        (string key, object value)? parameter = default,
+        List<(string key, object value)>? parameters = default
+    )
+    {
+        parameters ??= parameter is not null ? [parameter.Value] : [];
+
+        Mock.Get(dataFetcher).Verify(
+            df => df.Execute(
+                It.Is<string>(q => queryName == null || q == queryName),
+                It.Is<Dictionary<string, object>>(p =>
+                    parameters.All((kvp) => p.ContainsKey(kvp.key) && Equals(p[kvp.key], kvp.value))
+                )
+            )
+        );
+    }
 }
--- test/recipe/Baked.Test.Recipe.Service.Test/Reporting/MockingReportResults.cs
+++ test/recipe/Baked.Test.Recipe.Service.Test/Reporting/MockingReportResults.cs
@@ -1,9 +1,44 @@
+using Baked.Reporting;
+
 namespace Baked.Test.Reporting;
 
 public class MockingReportResults : TestServiceSpec
 {
     [Test]
-    [Ignore("not implemented")]
-    public async Task Will_do() =>
-      await this.ShouldFailAsync();
+    public void Mock_report_context_is_provided_during_tests()
+    {
+        var context = GiveMe.The<IReportContext>();
+
+        var getMock = () => Mock.Get(context);
+
+        getMock.ShouldNotThrow();
+    }
+
+    [Test]
+    public async Task Sets_up_mock_report_to_return_desired_data()
+    {
+        MockMe.TheReportContext(data: [[2, "test-1"], [1, "test-2"]]);
+        var reportSamples = GiveMe.The<ReportSamples>();
+
+        var result = await reportSamples.GetEntity("test");
+
+        result[0].Count.ShouldBe(2);
+        result[0].String.ShouldBe("test-1");
+        result[1].Count.ShouldBe(1);
+        result[1].String.ShouldBe("test-2");
+    }
+
+    [Test]
+    public async Task Verifies_execute_with_given_query_and_parameters()
+    {
+        var reportContext = MockMe.TheReportContext();
+        var reportSamples = GiveMe.The<ReportSamples>();
+
+        await reportSamples.GetEntity("test");
+
+        reportContext.VerifyExecute(
+            queryName: "entity",
+            parameter: ("string", "test%")
+        );
+    }
 }
