bc77eea436e026c497e3ec5bf514530700cd111e
dncsvr <disever10@alm.ku.edu.tr>
2025-02-03 10:17:21 +0300

    add string hash helper extensions

--- src/recipe/Baked.Recipe.Service/Authentication/StringExtensions.cs
+++ src/recipe/Baked.Recipe.Service/Authentication/StringExtensions.cs
@@ -9,9 +9,56 @@ public static partial class StringExtensions
     {
         using var sha256 = SHA256.Create();
 
-        return sha256.ComputeHash(Encoding.UTF8.GetBytes(str));
+        return sha256.ComputeHash(str.ToUtf8Bytes());
+    }
+
+    public static byte[] ToSHA1(this string str)
+    {
+        using var sha1 = SHA1.Create();
+
+        return sha1.ComputeHash(str.ToUtf8Bytes());
     }
 
     public static string ToBase64(this byte[] bytes) =>
         Convert.ToBase64String(bytes);
+
+    public static byte[] ToUtf8Bytes(this string @string) =>
+        Encoding.UTF8.GetBytes(@string);
+
+    public static string ToUtf8String(this byte[] bytes) =>
+        Encoding.UTF8.GetString(bytes);
+
+    public static string ToBase64(this byte[] bytes, bool urlEncode)
+    {
+        if (!urlEncode) { return bytes.ToBase64(); }
+
+        return bytes
+            .ToBase64()
+            .Replace('+', '-')
+            .Replace('/', '_')
+            .TrimEnd('=');
+    }
+
+    public static byte[] FromBase64(this string @string,
+        bool urlDecode = false
+    )
+    {
+        if (urlDecode)
+        {
+            @string = @string.Replace('_', '/').Replace('-', '+');
+            switch (@string.Length % 4)
+            {
+                case 2:
+                    @string += "==";
+                    break;
+                case 3:
+                    @string += "=";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return Convert.FromBase64String(@string);
+    }
 }
