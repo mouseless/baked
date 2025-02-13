3c0e6beec4f3470ea4f4bb9b4099d7912ce117c3
dncsvr <disever10@alm.ku.edu.tr>
2025-02-13 18:08:59 +0300

    move fetching data to schema component
    - throw 404 error when schema is not found

--- test/recipe/admin/components/Baked/Page.vue
+++ test/recipe/admin/components/Baked/Page.vue
@@ -1,33 +1,30 @@
 <template>
   <component
     :is="schema.$type"
-    v-if="data"
+    v-if="schema"
     :schema="schema"
-    :data="data"
   />
 </template>
 <script setup>
+
 const { params } = defineProps({
   params: {
     type: null,
     required: true
   }
 });
-
-const { public: { apiBaseURL: baseURL } } = useRuntimeConfig();
-
 const schema = ref();
-const data = ref();
+
+provide("params", params);
 
 onMounted(async() => {
-  schema.value = await import(`../../.baked/${params[0]}.json`);
-
-  data.value = await $fetch(
-    `${schema.value.path}/${params[1]}`,
-    {
-      baseURL,
-      headers: { Authorization: "token-jane" }
-    }
-  );
+  schema.value = await import(`../../.baked/${params[0]}.json`)
+    .catch(_ => {
+      throw createError({
+        statusCode: 404,
+        statusMessage: `'${params[0]}' Page Not Found`,
+        fatal: true
+      });
+    });
 });
 </script>
--- test/recipe/admin/components/Baked/Report.vue
+++ test/recipe/admin/components/Baked/Report.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-[1000px] mx-auto my-4">
     <Panel :header="schema.title">
-      <div class="grid grid-cols-2 gap-4">
+      <div v-if="data" class="grid grid-cols-2 gap-4">
         <component
           :is="prop.component.$type"
           v-for="prop in props"
@@ -14,10 +14,22 @@
   </div>
 </template>
 <script setup>
-const { schema, data } = defineProps({
-  schema: { type: null, required: true },
-  data: { type: null, required: true }
+const { schema } = defineProps({
+  schema: { type: null, required: true }
 });
 
-const { props } = schema;
+const { props, path } = schema;
+const { public: { apiBaseURL: baseURL } } = useRuntimeConfig();
+const params = inject("params");
+const data = ref();
+
+onMounted(async() => {
+  data.value = await $fetch(
+    `${path}/${params[1]}`,
+    {
+      baseURL,
+      headers: { Authorization: "token-jane" }
+    }
+  );
+});
 </script>
