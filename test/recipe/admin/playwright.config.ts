edb20be8fca1e2914b0e879d2c22efc4612a8777
cihandeniz <cihan@gcbrains.com>
2025-02-19 18:41:34 +0300

    improve github workflow for admin

--- test/recipe/admin/playwright.config.ts
+++ test/recipe/admin/playwright.config.ts
@@ -4,6 +4,7 @@ import type { ConfigOptions } from "@nuxt/test-utils/playwright";
 
 export default defineConfig<ConfigOptions>({
   testDir: "./pages/specs",
+  reporter: "list",
   use: {
     nuxt: {
       rootDir: fileURLToPath(new URL(".", import.meta.url))
