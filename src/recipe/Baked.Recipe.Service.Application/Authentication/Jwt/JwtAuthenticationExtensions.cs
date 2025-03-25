3a87d85f46f915aab39f944fab3080f06a26f0e1
dncsvr <disever10@alm.ku.edu.tr>
2025-03-25 17:13:56 +0300

    add token builder test case
    - move fixed bearer token tests to own folder
    - fix format

--- src/recipe/Baked.Recipe.Service.Application/Authentication/Jwt/JwtAuthenticationExtensions.cs
+++ src/recipe/Baked.Recipe.Service.Application/Authentication/Jwt/JwtAuthenticationExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Baked.Authentication;
 using Baked.Authentication.Jwt;
+using Baked.Testing;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Shouldly;
 using System.IdentityModel.Tokens.Jwt;
@@ -14,4 +15,28 @@ public static class JwtAuthenticationExtensions
 
     public static void ShouldBeJwt(this string token) =>
        new JwtSecurityTokenHandler().CanReadToken(token).ShouldBeTrue();
+
+    public static void TheJwtSettings(this Mocker mockMe,
+        string? key = null,
+        string? issuer = null,
+        string? audience = null,
+        int? expirationForAccess = null,
+        int? expirationForRefresh = null
+    )
+    {
+        key ??= "f7fee531f838473b83f1f73a808acfb1";
+        issuer ??= "YK";
+        audience ??= "UI";
+        expirationForAccess ??= 5;
+        expirationForRefresh ??= 50;
+
+        mockMe.ASetting(key: "Authentication:Jwt:Key", value: key);
+        mockMe.ASetting(key: "Authentication:Jwt:Issuer", value: issuer);
+        mockMe.ASetting(key: "Authentication:Jwt:Audience", value: audience);
+        mockMe.ASetting(key: "Authentication:Jwt:ExpiresInMinutes:Access", value: expirationForAccess);
+        mockMe.ASetting(key: "Authentication:Jwt:ExpiresInMinutes:Refresh", value: expirationForRefresh);
+    }
+
+    public static JwtSecurityToken TheSecurityToken(this Stubber _, string credential) =>
+        new JwtSecurityTokenHandler().ReadJwtToken(credential);
 }
---
+++ test/recipe/Baked.Test.Recipe.Service.Test/Authentication/Jwt/BuildingToken.cs
@@ -0,0 +1,22 @@
+﻿using Baked.Authentication;
+
+namespace Baked.Test.Authentication.Jwt;
+
+public class BuildingToken : TestServiceSpec
+{
+    public void Creates_token_with_given_claims_and_jwt_settings()
+    {
+        MockMe.TheJwtSettings(issuer: "Iss", audience: "Aud", expirationForAccess: 5);
+        MockMe.TheTime(new(2025, 03, 25, 16, 30, 0));
+        var tokenBuilder = GiveMe.The<ITokenBuilder>();
+
+        var token = tokenBuilder.Build("Jwt", [new("User", "User")]);
+
+        var access = GiveMe.TheSecurityToken(token);
+        access.Claims.ShouldContain(c => c.Type == "User" && c.Value == "User");
+        access.Payload.Iss.ShouldBe("Iss");
+        access.Payload.Aud.ShouldContain("Aud");
+        access.Payload.IssuedAt.ToLocalTime().ShouldBe(DateTime.Parse("2025-03-25T16:30:00"));
+        access.Payload.ValidTo.ToLocalTime().ShouldBe(DateTime.Parse("2025-03-25T16:05:00"));
+    }
+}
