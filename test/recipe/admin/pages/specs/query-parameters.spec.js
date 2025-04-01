81aa04317a300f9cc9d2c23b533a45d7330db828
cihandeniz <cihan@gcbrains.com>
2025-04-01 17:51:23 +0300

    add missing test cases for query parameters

--- test/recipe/admin/pages/specs/query-parameters.spec.js
+++ test/recipe/admin/pages/specs/query-parameters.spec.js
@@ -49,7 +49,7 @@ test("query string is set from input", async({page}) => {
   await component.getByTestId("required-with-default").fill("value 1");
   await component.getByTestId("required").fill("value 2");
   await component.getByTestId("optional").fill("value 3");
-  await page.waitForURL(/requiredWithDefault.*required.*optional/); // wait above fills to take effect
+  await page.waitForURL(/requiredWithDefault.*required.*optional/); // wait for above fills to take effect
 
   const params = new URLSearchParams(new URL(page.url()).search);
   expect(params.get("requiredWithDefault")).toBe("value 1");
@@ -64,6 +64,30 @@ test("query string is set to input", async({page}) => {
   await expect(component.getByTestId("required-with-default")).toHaveValue("1");
   await expect(component.getByTestId("required")).toHaveValue("2");
   await expect(component.getByTestId("optional")).toHaveValue("3");
+});
+
+test("replaces route until defaults are set", async({page}) => {
+  await page.waitForURL(/requiredWithDefault/); // wait for default value to be set
+
+  const state = await page.evaluate(() => history.state);
+
+  expect(state.back).toBeNull();
+});
+
+test("pushes route when a parameter changes", async({page}) => {
+  const component = page.getByTestId(id.component);
+
+  await component.getByTestId("required").fill("value 1");
+  await page.waitForURL(/value\+1/); // wait for above fill to take effect
+
+  const state = await page.evaluate(() => history.state);
+  expect(state.back).toBe("/specs/query-parameters?requiredWithDefault=default+value");
+
+  await component.getByTestId("required").fill("value 2");
+  await page.waitForURL(/value\+2/); // wait for above fill to take effect
+
+  const state2 = await page.evaluate(() => history.state);
+  expect(state2.back).toBe("/specs/query-parameters?requiredWithDefault=default+value&required=value+1");
 });
 
 test("ready when all required are set", async({page}) => {
