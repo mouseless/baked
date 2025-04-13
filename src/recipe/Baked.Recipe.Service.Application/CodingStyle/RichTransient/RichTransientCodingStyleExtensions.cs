6de26b18b6f5334a555158d616d649be5cde7eaa
cihandeniz <cihan@gcbrains.com>
2025-04-13 17:32:32 +0300

    remove extra formatting

--- src/recipe/Baked.Recipe.Service.Application/CodingStyle/RichTransient/RichTransientCodingStyleExtensions.cs
+++ src/recipe/Baked.Recipe.Service.Application/CodingStyle/RichTransient/RichTransientCodingStyleExtensions.cs
@@ -52,7 +52,7 @@ public static class RichTransientCodingStyleExtensions
     )
     {
         var initializer = type.GetMembers().Methods.Having<InitializerAttribute>().Single();
-        var byIds = $"{valueExpression}.Select(id => {$"new{type.Name.Pascalize()}().{initializer.Name}(id)"})";
+        var byIds = $"{valueExpression}.Select(id => new{type.Name.Pascalize()}().{initializer.Name}(id))";
         if (initializer.DefaultOverload.ReturnType.IsAssignableTo<Task>())
         {
             byIds = $"(await Task.WhenAll({byIds}))";
