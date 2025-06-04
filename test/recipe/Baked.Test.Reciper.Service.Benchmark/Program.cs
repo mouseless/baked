5e4744990535a9ccf0bd17a07bab265474911626
dncsvr <disever10@alm.ku.edu.tr>
2025-06-04 16:43:43 +0300

    remove benchmark project

--- test/recipe/Baked.Test.Reciper.Service.Benchmark/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Baked.Test;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Running;
-
-BenchmarkRunner.Run<Testing>(config: DefaultConfig.Instance.WithArtifactsPath(@"./.benchmark"));
