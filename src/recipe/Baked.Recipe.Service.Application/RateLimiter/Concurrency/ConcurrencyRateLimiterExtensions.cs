2f0e41fb013b62e9d4db008a041b24059dfe0acf
dncsvr <disever10@alm.ku.edu.tr>
2025-06-04 16:06:56 +0300

    minor edits

--- src/recipe/Baked.Recipe.Service.Application/RateLimiter/Concurrency/ConcurrencyRateLimiterExtensions.cs
+++ src/recipe/Baked.Recipe.Service.Application/RateLimiter/Concurrency/ConcurrencyRateLimiterExtensions.cs
@@ -1,12 +1,12 @@
 ﻿using Baked.RateLimiter;
 using Baked.RateLimiter.Concurrency;
-using System.Threading.RateLimiting;
 
 namespace Baked;
 
 public static class ConcurrencyRateLimiterExtensions
 {
     public static ConcurrencyRateLimiterFeature Concurrency(this RateLimiterConfigurator _,
-        ConcurrencyLimiterOptions? options = default
-    ) => new(options ?? new() { PermitLimit = 5, QueueLimit = 10 });
+        int? permitLimit = default,
+        int? queueLimit = default
+    ) => new(new() { PermitLimit = permitLimit ?? 5, QueueLimit = queueLimit ?? 10 });
 }
