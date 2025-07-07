fb3282a1b40053b7f053c435aa7cc6da23d048be
dncsvr <disever10@alm.ku.edu.tr>
2025-07-07 13:09:40 +0300

    fix failing tests

--- test/recipe/admin/pages/specs/data-panel.vue
+++ test/recipe/admin/pages/specs/data-panel.vue
@@ -12,7 +12,7 @@ const variants = [
   {
     name: "Base",
     descriptor: giveMe.aDataPanel({
-      title: { type: "Inline", value: "DataPanel.Title" },
+      title: { type: "Inline", value: "Title" },
       collapsed: false,
       content: giveMe.anExpected({ testId: "content", value: "TEST DATA" })
     })
