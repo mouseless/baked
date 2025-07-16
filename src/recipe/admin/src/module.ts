58b33600418b651e20587f008d2b01516b4cbc83
dncsvr <disever10@alm.ku.edu.tr>
2025-07-16 11:52:15 +0300

    add actions menu styles and variables

--- src/recipe/admin/src/module.ts
+++ src/recipe/admin/src/module.ts
@@ -71,6 +71,7 @@ export default defineNuxtModule<ModuleOptions>({
     // by pushing instead of setting, it allows custom css
     _nuxt.options.css.push("primeicons/primeicons.css");
     _nuxt.options.css.push(resolver.resolve("./runtime/assets/theme/datatable.css"));
+    _nuxt.options.css.push(resolver.resolve("./runtime/assets/theme/menu.css"));
     _nuxt.options.css.push(resolver.resolve("./runtime/assets/overrides.css"));
 
     // below settings cannot be overriden
