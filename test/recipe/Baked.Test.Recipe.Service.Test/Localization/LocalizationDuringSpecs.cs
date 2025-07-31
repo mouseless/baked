f173aa3be68df73adf88869037ccea07a1b4028a
cihandeniz <cihan@gcbrains.com>
2025-07-31 12:07:42 +0300

    add backend localization tests

---
+++ test/recipe/Baked.Test.Recipe.Service.Test/Localization/LocalizationDuringSpecs.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Localization;
+
+namespace Baked.Test.Localization;
+
+public class LocalizationDuringSpecs : TestServiceSpec
+{
+    [Test]
+    public void Localizer_is_avaliable_during_specs()
+    {
+        var l = GiveMe.The<IStringLocalizer>();
+
+        var action = () => l[GiveMe.AString()];
+
+        action.ShouldNotThrow();
+    }
+
+    [Test]
+    public void It_returns_the_given_key()
+    {
+        var localizationSamples = GiveMe.The<LocalizationSamples>();
+
+        string message = localizationSamples.GetLocaleString();
+
+        message.ShouldBe("test");
+    }
+
+    [Test]
+    public void It_supports_formatting()
+    {
+        var localizationSamples = GiveMe.The<LocalizationSamples>();
+
+        string message = localizationSamples.GetParameterized("test");
+
+        message.ShouldBe("Parameter value is 'test'");
+    }
+}
--- test/recipe/Baked.Test.Recipe.Service/Localization/LocalizationSamples.cs
+++ test/recipe/Baked.Test.Recipe.Service/Localization/LocalizationSamples.cs
@@ -1,9 +1,12 @@
 using Microsoft.Extensions.Localization;
 
-namespace Baked.Test.ExceptionHandling;
+namespace Baked.Test.Localization;
 
 public class LocalizationSamples(IStringLocalizer _l)
 {
     public string GetLocaleString() =>
         _l["test"];
+
+    public string GetParameterized(string param) =>
+        _l["Parameter value is '{0}'", param];
 }
