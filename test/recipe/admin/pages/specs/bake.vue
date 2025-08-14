a7eb9f03ba9494c62cc8050918e549a8d37972f4
cihandeniz <cihan@gcbrains.com>
2025-08-14 14:58:28 +0300

    fix self-review notes

--- test/recipe/admin/pages/specs/bake.vue
+++ test/recipe/admin/pages/specs/bake.vue
@@ -34,17 +34,20 @@ const variants = [
       testId: "test",
       showDataParams: true,
       data: {
-        type: "Composite",
+        type: "Composite", // merges ["computed"] and ["remote-1", "remote-2"]
         parts: [
-          { type: "Inline", value: { inline: "inline" } },
-          { type: "Computed", composable: "useFakeComputed", args: ["computed"] },
+          { type: "Computed", composable: "useFakeComputed", args: ["computed"] }, // provides "computed"
           { type: "Injected", key: "ParentData" },
+          { type: "Inline", value: { inline: "inline" } },
           {
             type: "Remote",
             path: "/report/wide",
             query: {
-              type: "Inline",
-              value: { requiredWithDefault: "remote-1", required: "remote-2" }
+              type: "Composite", // merges ["remote-1"] and ["remote-2"]
+              parts: [
+                { type: "Inline", value: { requiredWithDefault: "remote-1" } }, // provides "remote-1"
+                { type: "Inline", value: { required: "remote-2" } } // provides "remote-2"
+              ]
             },
             headers: {
               type: "Inline",
--- unreleased.md
+++ unreleased.md
@@ -2,4 +2,8 @@
 
 ## Improvement
 
-- Export file names can now include parameter values
+- `Bake.vue` now provides data descriptor to the component, inject it using
+  `context.dataDescriptor()`
+- `useDataFetcher` now provides a way to get parameter values from data
+  descriptor via `fetchParameters` method
+- `DataTable.vue` now includes parameter values in export file names
