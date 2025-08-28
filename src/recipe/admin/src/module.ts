7bc620e4dfa299d8ece4e5d989aa60ab92f745ce
Liva <ahmetlivacengiz@gmail.com>
2025-08-28 19:02:01 +0300

    add new screen sizes to `useBreakpoints`

--- src/recipe/admin/src/module.ts
+++ src/recipe/admin/src/module.ts
@@ -47,6 +47,8 @@ export interface UseBreakpointOptions {
 }
 
 export interface ScreenOptions {
+  "2xs": String,
+  xs: String,
   sm: String,
   md: String,
   lg: String,
@@ -75,12 +77,14 @@ export default defineNuxtModule<ModuleOptions>({
 
     _options.composables.useBreakpoint ||= {};
     _options.composables.useBreakpoint.screens ||= {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-      '3xl': '1920px',
+      "2xs": "340px",
+      "xs": "480px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
     };
 
     // passing module's options to runtime config for further access
--- src/recipe/admin/src/runtime/composables/useBreakpoints.js
+++ src/recipe/admin/src/runtime/composables/useBreakpoints.js
@@ -1,22 +1,27 @@
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { useRuntimeConfig } from "#app";
 
-export function useBreakpoint() {
+export default function() {
   const { public: { composables } } = useRuntimeConfig();
 
   const sizes = composables.useBreakpoint.screens;
 
+  const is2xs = ref(false);
   const isXs = ref(false);
   const isSm = ref(false);
   const isMd = ref(false);
   const isLg = ref(false);
   const isXl = ref(false);
   const is2xl = ref(false);
+  const is3xl = ref(false);
+  const isMax2xs = ref(false);
+  const isMaxXs = ref(false);
   const isMaxSm = ref(false);
   const isMaxMd = ref(false);
   const isMaxLg = ref(false);
   const isMaxXl = ref(false);
-
+  const isMax2xl = ref(false);
+  const isMax3xl = ref(false);
   const removePx = size => typeof size === "string" ? parseInt(size.replace("px", ""), 10) : size;
 
   const update = () => {
@@ -25,16 +30,22 @@ export function useBreakpoint() {
       sizes[key] = removePx(sizes[key]);
     });
 
-    isXs.value = width < sizes.md;
+    is2xs.value = width >= sizes["2xs"];
+    isXs.value = width >= sizes.xs;
     isSm.value = width >= sizes.sm;
     isMd.value = width >= sizes.md;
     isLg.value = width >= sizes.lg;
     isXl.value = width >= sizes.xl;
     is2xl.value = width >= sizes["2xl"];
-    isMaxSm.value = width <= sizes.md;
-    isMaxMd.value = width <= sizes.lg;
-    isMaxLg.value = width <= sizes.xl;
-    isMaxXl.value = width <= sizes["2xl"];
+    is3xl.value = width >= sizes["3xl"];
+    isMax2xs.value = width < sizes["2xs"];
+    isMaxXs.value = width < sizes.xs;
+    isMaxSm.value = width < sizes.sm;
+    isMaxMd.value = width < sizes.md;
+    isMaxLg.value = width < sizes.lg;
+    isMaxXl.value = width < sizes.xl;
+    isMax2xl.value = width < sizes["2xl"];
+    isMax3xl.value = width < sizes["3xl"];
   };
 
   onMounted(() => {
@@ -47,15 +58,21 @@ export function useBreakpoint() {
   });
 
   return {
+    is2xs,
     isXs,
     isSm,
     isMd,
     isLg,
     isXl,
     is2xl,
+    is3xl,
+    isMax2xs,
+    isMaxXs,
     isMaxSm,
     isMaxMd,
     isMaxLg,
-    isMaxXl
+    isMaxXl,
+    isMax2xl,
+    isMax3xl
   };
 }
