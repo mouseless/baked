88b99cdce78b2422de13c0a8212896b883d6526e
cihandeniz <cihan@gcbrains.com>
2025-09-19 19:04:09 +0300

    update imported styles

--- src/recipe/admin/src/module.ts
+++ src/recipe/admin/src/module.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, addComponentsDir, addImportsDir, addPlugin, createResolver, installModule } from "@nuxt/kit";
+import { addComponentsDir, addImportsDir, addPlugin, createResolver, defineNuxtModule, installModule } from "@nuxt/kit";
 import type { NuxtI18nOptions } from "@nuxtjs/i18n";
 import { pathToFileURL } from "url";
 
@@ -96,8 +96,9 @@ export default defineNuxtModule<ModuleOptions>({
     // by pushing instead of setting, it allows custom css
     _nuxt.options.css.push("primeicons/primeicons.css");
     _nuxt.options.css.push(resolver.resolve("./runtime/assets/theme/datatable.css"));
-    _nuxt.options.css.push(resolver.resolve("./runtime/assets/theme/popover.css"));
     _nuxt.options.css.push(resolver.resolve("./runtime/assets/theme/menu.css"));
+    _nuxt.options.css.push(resolver.resolve("./runtime/assets/theme/paginator.css"));
+    _nuxt.options.css.push(resolver.resolve("./runtime/assets/theme/popover.css"));
     _nuxt.options.css.push(resolver.resolve("./runtime/assets/overrides.css"));
 
     // below settings cannot be overriden
