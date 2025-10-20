0755dec1d1110dacdf5c8e9e3604a99dce5929d7
dncsvr <disever10@alm.ku.edu.tr>
2025-10-20 15:37:34 +0300

    minor format edits

--- core/src/Baked/Orm/AutoMap/TypeModelTypeSourceTemplate.cs
+++ core/src/Baked/Orm/AutoMap/TypeModelTypeSourceTemplate.cs
@@ -22,12 +22,11 @@ public class TypeModelTypeSourceTemplate(DomainModel _domain)
 
         IEnumerable<Type> ITypeSource.GetTypes()
         {
-            {{If(!_entities.Value.Any(), () =>
-                "return Array.Empty<Type>();",
-            @else: () =>
-                ForEach(_entities.Value, entity =>
-                    $$"""yield return typeof({{entity.CSharpFriendlyFullName}});"""
-                )
+            {{If(!_entities.Value.Any(), () => """
+            return Array.Empty<Type>();
+            """, @else: () => ForEach(_entities.Value, entity => $$"""
+            yield return typeof({{entity.CSharpFriendlyFullName}});
+            """)
             )}}
         }
 
