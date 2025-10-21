594ec708d57c693cbd1381b1aa4fc14d4ffc33c2
cihandeniz <cihan@gcbrains.com>
2025-10-21 12:06:00 +0300

    remove `Lazy` prefix and custom styles
    - update release notes

--- ui/src/runtime/composables/useComponentResolver.js
+++ ui/src/runtime/composables/useComponentResolver.js
@@ -1,10 +1,7 @@
-
 import * as components from "@@/.baked/components";
 
 export default function() {
   function resolve(type, fallback) {
-    type = `Lazy${type}`;
-
     return components[type]
       ? components[type]
       : components[fallback];
