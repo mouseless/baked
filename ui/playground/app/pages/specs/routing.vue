654bfe1baf2bebd06c02aa7cd9078271a341c8e8
dncsvr <disever10@alm.ku.edu.tr>
2025-11-13 16:35:41 +0300

    minor edits

--- ui/playground/app/pages/specs/routing.vue
+++ ui/playground/app/pages/specs/routing.vue
@@ -7,21 +7,23 @@
         ⬆️  click the link below to navigate to a page with route parameter ⬆️
       </span>
     </Message>
-    <Button
-      as="routerLink"
-      label="/page/with/route/pageWithRoute"
-      to="/page/with/route/pageWithRoute"
-    />
-    <Button
-      as="routerLink"
-      label="/parent/a6db0515-a885-4cf6-8343-c2c4a0510392"
-      to="/parent/a6db0515-a885-4cf6-8343-c2c4a0510392"
-    />
-    <Button
-      as="routerLink"
-      label="/parent/1/children/2"
-      to="/parent/1/children/2"
-    />
+    <div class="flex gap-4 flex-col">
+      <Button
+        as="routerLink"
+        label="/page/with/route/pageWithRoute"
+        to="/page/with/route/pageWithRoute"
+      />
+      <Button
+        as="routerLink"
+        label="/parent/a6db0515-a885-4cf6-8343-c2c4a0510392"
+        to="/parent/a6db0515-a885-4cf6-8343-c2c4a0510392"
+      />
+      <Button
+        as="routerLink"
+        label="/parent/1/children/2"
+        to="/parent/1/children/2"
+      />
+    </div>
   </UiSpec>
 </template>
 <script setup>
