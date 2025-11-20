cd3b336cccd157ab602a548b95f3e91736161b60
dncsvr <disever10@alm.ku.edu.tr>
2025-11-21 00:27:51 +0300

    minor format edit

--- core/src/Baked/Ui/LocalAction.cs
+++ core/src/Baked/Ui/LocalAction.cs
@@ -1,6 +1,7 @@
 ﻿namespace Baked.Ui;
 
-public record LocalAction(string Composable) : IAction
+public record LocalAction(string Composable)
+    : IAction
 {
     public string Type => "Local";
     public string Composable { get; set; } = Composable;
