f616689ba5fb766221cdbe6d0135a45a106c318a
dncsvr <disever10@alm.ku.edu.tr>
2025-12-29 15:35:35 +0300

    init `bug/generic-type-property-causes-fail` !!! BUILD FAILING !!!
    - add test class to reproduce bug

---
+++ core/test/Baked.Test/Business/GenericClass.cs
@@ -0,0 +1,26 @@
+﻿namespace Baked.Test.Business;
+
+public class GenericClass<T>
+{
+    internal async Task<GenericClass<T>> With()
+    {
+        await Task.Delay(1000);
+
+        Method(new());
+
+        return this;
+    }
+
+    void Method(Dictionary<int, PrivateRecord> _)
+    { }
+
+    record PrivateRecord(string Text);
+}
+
+public record DataType(string Type);
+
+public class DependentClass(Func<GenericClass<DataType>> _newDependency)
+{
+    public async Task<GenericClass<DataType>> Method() =>
+        await _newDependency().With();
+}
