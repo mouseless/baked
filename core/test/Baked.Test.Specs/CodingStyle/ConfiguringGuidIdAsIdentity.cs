8dae495d65e200a709bd361d7ceef83c534285f0
dncsvr <disever10@alm.ku.edu.tr>
2026-01-13 11:55:01 +0300

    move id tests to a coding style test fixture

---
+++ core/test/Baked.Test.Specs/CodingStyle/ConfiguringGuidIdAsIdentity.cs
@@ -0,0 +1,26 @@
+﻿using Baked.Playground.Orm;
+
+using NHConfiguration = NHibernate.Cfg.Configuration;
+
+namespace Baked.Test.CodingStyle;
+
+public class ConfiguringGuidIdAsIdentity : TestSpec
+{
+    [Test]
+    public void Id_is_always_guid()
+    {
+        var configuration = GiveMe.The<NHConfiguration>();
+
+        configuration.GetClassMapping(typeof(Entity)).Identifier.Type.Name.ShouldBe("Guid");
+    }
+
+    [Test]
+    public void Id_is__property_named_Id()
+    {
+        var configuration = GiveMe.The<NHConfiguration>();
+
+        var actual = configuration.GetClassMapping(typeof(Entity)).Identifier.ColumnIterator.FirstOrDefault();
+        actual.ShouldNotBeNull();
+        actual.Text.ShouldBe("Id");
+    }
+}
