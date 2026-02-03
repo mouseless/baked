1fa640ae0cfd9c9e53e3e0bcbd81bdc022db03f3
dncsvr <disever10@alm.ku.edu.tr>
2026-02-04 00:22:33 +0300

    update entity extension sample namespace

--- core/playground/Baked.Playground/CodingStyle/LocatableExtension/EntityExtension.cs
+++ core/playground/Baked.Playground/CodingStyle/LocatableExtension/EntityExtension.cs
@@ -1,7 +1,7 @@
 ﻿using Baked.Business;
 using Baked.Playground.Orm;
 
-namespace Baked.Playground.CodingStyle.EntityExtensionViaComposition;
+namespace Baked.Playground.CodingStyle.LocatableExtension;
 
 public class EntityExtension
 {
