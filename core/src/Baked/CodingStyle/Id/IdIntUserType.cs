f08c9466099aeeabe85604808d473ad5e82f82bd
Sefer Mirza <sefermirza@gmail.com>
2026-02-18 17:01:17 +0300

    change id int type to int32 from uint

--- core/src/Baked/CodingStyle/Id/IdIntUserType.cs
+++ core/src/Baked/CodingStyle/Id/IdIntUserType.cs
@@ -6,11 +6,10 @@ namespace Baked.CodingStyle.Id;
 
 public class IdIntUserType : IdUserTypeBase
 {
-    public override SqlType[] SqlTypes => [SqlTypeFactory.UInt32];
-    public override NullableType NHibernateType => NHibernateUtil.UInt32;
+    public override SqlType[] SqlTypes => [SqlTypeFactory.Int32];
+    public override NullableType NHibernateType => NHibernateUtil.Int32;
 
     public override object Convert(object value) =>
-        value is uint @uint ? @uint :
-        value is int @int ? (uint)@int :
-        uint.Parse($"{value}");
+        value is int @int ? @int :
+        int.Parse($"{value}");
 }
