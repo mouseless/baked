a61ffba21cf5c0741ae70d67df11f612b4d931df
Sefer Mirza <sefermirza@gmail.com>
2026-03-11 22:33:55 +0300

    migrate Business, Core, Reporting extensions to extension member using

--- core/src/Baked/Core/CoreExtensions.cs
+++ core/src/Baked/Core/CoreExtensions.cs
@@ -11,154 +11,184 @@ namespace Baked;
 
 public static class CoreExtensions
 {
-    public static void AddCore(this List<IFeature> features, Func<CoreConfigurator, IFeature<CoreConfigurator>> configure) =>
-        features.Add(configure(new()));
-
-    public static Dictionary<string, string> Merge(this Dictionary<string, string>? dictionary, Dictionary<string, string>? input)
+    extension(List<IFeature> features)
     {
-        dictionary ??= [];
-        input ??= [];
+        public void AddCore(Func<CoreConfigurator, IFeature<CoreConfigurator>> configure) =>
+            features.Add(configure(new()));
+    }
 
-        foreach (var (key, value) in input)
+    extension(Dictionary<string, string>? dictionary)
+    {
+        public Dictionary<string, string> Merge(Dictionary<string, string>? input)
         {
-            if (!dictionary.ContainsKey(key))
+            dictionary ??= [];
+            input ??= [];
+
+            foreach (var (key, value) in input)
             {
-                dictionary[key] = value;
+                if (!dictionary.ContainsKey(key))
+                {
+                    dictionary[key] = value;
+                }
+            }
+
+            return dictionary;
+        }
+    }
+
+    extension(string guidStr)
+    {
+        public Guid ToGuid() =>
+            Guid.Parse(guidStr);
+    }
+
+    extension(Stubber giveMe)
+    {
+        public Dictionary<string, string> ADictionary() =>
+            giveMe.ADictionary<string, string>();
+
+        public Uri AUrl(
+            string? url = default
+        )
+        {
+            url ??= $"https://www.{Regex.Replace(giveMe.AGuid().ToString(), "[0-9]", "x")}.com";
+
+            return new(url);
+        }
+
+        public int AnInteger() =>
+            42;
+
+        public string AnEmail() =>
+            "info@test.com";
+
+        public string AString(
+            string? value = default
+        ) => value ?? "test string";
+
+        public DateTime ADateTime(int year = 2023,
+            int month = 9,
+            int day = 17,
+            int hour = 13,
+            int minute = 29,
+            int second = 00
+        ) => new(year, month, day, hour, minute, second);
+        public Dictionary<TKey, TValue> ADictionary<TKey, TValue>(params IEnumerable<(TKey, TValue)> pairs)
+            where TKey : notnull
+        => pairs.ToDictionary(pair => pair.Item1, pair => pair.Item2);
+
+        public Guid AGuid(
+            string? starts = default
+        )
+        {
+            starts ??= string.Empty;
+
+            const string template = "4d13bbe0-07a4-4b64-9d31-8fef958fbef1";
+
+            return Guid.Parse($"{starts}{template[starts.Length..]}");
+        }
+
+        public PropertyInfo? ThePropertyOf<T>(string name) =>
+            typeof(T).GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+        public MethodInfo? TheMethodOf<T>(string name) =>
+            typeof(T).GetMethod(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+    }
+
+    extension(string? @string)
+    {
+        public void ShouldBe(object? expected, string format) =>
+        @string.ShouldBe(string.Format(format, expected));
+    }
+
+    extension(Uri? uri)
+    {
+        public void ShouldBe(string urlString) =>
+            uri?.ToString().ShouldBe(urlString);
+    }
+
+    extension<TKey, TValue>(Dictionary<TKey, TValue> dictionary) where TKey : notnull
+    {
+        public void ShouldContainKeys(params TKey[] keys)
+        {
+            foreach (var key in keys)
+            {
+                dictionary.ShouldContainKey(key);
             }
         }
 
-        return dictionary;
-    }
-
-    public static int AnInteger(this Stubber _) =>
-        42;
-
-    public static string AnEmail(this Stubber _) =>
-        "info@test.com";
-
-    public static string AString(this Stubber _,
-        string? value = default
-    ) => value ?? "test string";
-
-    public static Guid ToGuid(this string guidStr) =>
-        Guid.Parse(guidStr);
-
-    public static DateTime ADateTime(this Stubber _,
-        int year = 2023,
-        int month = 9,
-        int day = 17,
-        int hour = 13,
-        int minute = 29,
-        int second = 00
-    ) => new(year, month, day, hour, minute, second);
-
-    public static Dictionary<string, string> ADictionary(this Stubber giveMe) => giveMe.ADictionary<string, string>();
-    public static Dictionary<TKey, TValue> ADictionary<TKey, TValue>(this Stubber _, params IEnumerable<(TKey, TValue)> pairs)
-        where TKey : notnull
-    => pairs.ToDictionary(pair => pair.Item1, pair => pair.Item2);
-
-    public static Guid AGuid(this Stubber _,
-        string? starts = default
-    )
-    {
-        starts ??= string.Empty;
-
-        const string template = "4d13bbe0-07a4-4b64-9d31-8fef958fbef1";
-
-        return Guid.Parse($"{starts}{template[starts.Length..]}");
-    }
-
-    public static Uri AUrl(this Stubber giveMe,
-        string? url = default
-    )
-    {
-        url ??= $"https://www.{Regex.Replace(giveMe.AGuid().ToString(), "[0-9]", "x")}.com";
-
-        return new(url);
-    }
-
-    public static void ShouldBe(this string? @string, object? expected, string format) =>
-        @string.ShouldBe(string.Format(format, expected));
-
-    public static void ShouldBe(this Uri? uri, string urlString) =>
-        uri?.ToString().ShouldBe(urlString);
-
-    public static void ShouldContainKeys<TKey, TValue>(this Dictionary<TKey, TValue> dictionary,
-        params TKey[] keys
-    ) where TKey : notnull
-    {
-        foreach (var key in keys)
+        public void ValuesShouldBe(params TValue[] values)
         {
-            dictionary.ShouldContainKey(key);
+            foreach (var value in dictionary.Values)
+            {
+                value.ShouldBe(values.FirstOrDefault());
+                values = [.. values.Skip(1)];
+            }
         }
     }
 
-    public static void ShouldDeeplyBe(this object? payload, object? json,
-        bool useSystemTextJson = false
-    ) => payload
-        .ToJsonString(useSystemTextJson: useSystemTextJson)
-        .ShouldBe(json.ToJsonString(useSystemTextJson: useSystemTextJson));
-
-    [return: NotNullIfNotNull("payload")]
-    public static string? ToJsonString(this object? payload,
-        bool useSystemTextJson = false
-    ) => payload is null ? null :
-        useSystemTextJson ? System.Text.Json.JsonSerializer.Serialize(payload) :
-        JsonConvert.SerializeObject(payload);
-
-    [return: NotNullIfNotNull("payload")]
-    public static object? ToJsonObject(this object? payload) =>
-        JsonConvert.DeserializeObject(payload.ToJsonString() ?? string.Empty);
-
-    public static PropertyInfo? ThePropertyOf<T>(this Stubber _, string name) =>
-        typeof(T).GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-
-    public static void ShouldBe<T>(this Type type) =>
-        type.ShouldBe(typeof(T));
-
-    public static void ShouldBeAbstract(this PropertyInfo property)
+    extension(object? payload)
     {
-        var getMethod = property.GetGetMethod(true);
+        public void ShouldDeeplyBe(object? json,
+            bool useSystemTextJson = false
+        ) => payload
+            .ToJsonString(useSystemTextJson: useSystemTextJson)
+            .ShouldBe(json.ToJsonString(useSystemTextJson: useSystemTextJson));
 
-        getMethod.ShouldNotBeNull();
-        getMethod.ShouldBeAbstract();
+        [return: NotNullIfNotNull("payload")]
+        public string? ToJsonString(
+            bool useSystemTextJson = false
+        ) => payload is null ? null :
+            useSystemTextJson ?
+            System.Text.Json.JsonSerializer.Serialize(payload) :
+            JsonConvert.SerializeObject(payload);
+
+        [return: NotNullIfNotNull("payload")]
+        public object? ToJsonObject() =>
+            JsonConvert.DeserializeObject(payload.ToJsonString() ?? string.Empty);
     }
 
-    public static void ShouldBeVirtual(this PropertyInfo property)
+    extension(Type type)
     {
-        var getMethod = property.GetGetMethod(true);
-
-        getMethod.ShouldNotBeNull();
-        getMethod.ShouldBeVirtual();
+        public void ShouldBe<T>() =>
+            type.ShouldBe(typeof(T));
     }
 
-    public static MethodInfo? TheMethodOf<T>(this Stubber _, string name) =>
-        typeof(T).GetMethod(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-
-    public static void ShouldBeAbstract(this MethodInfo method)
+    extension(PropertyInfo property)
     {
-        method.IsAbstract.ShouldBeTrue();
-    }
-
-    public static void ShouldBeVirtual(this MethodInfo method)
-    {
-        method.IsVirtual.ShouldBeTrue();
-    }
-
-    public static void ShouldHaveOneParameter<T>(this MethodInfo method)
-    {
-        method.GetParameters().Length.ShouldBe(1);
-        method.GetParameters().First().ParameterType.ShouldBe<T>();
-    }
-
-    public static void ValuesShouldBe<TKey, TValue>(this Dictionary<TKey, TValue> dictionary,
-        params TValue[] values
-    ) where TKey : notnull
-    {
-        foreach (var value in dictionary.Values)
+        public void ShouldBeAbstract()
         {
-            value.ShouldBe(values.FirstOrDefault());
-            values = [.. values.Skip(1)];
+            var getMethod = property.GetGetMethod(true);
+
+            getMethod.ShouldNotBeNull();
+            getMethod.ShouldBeAbstract();
+        }
+
+        public void ShouldBeVirtual()
+        {
+            var getMethod = property.GetGetMethod(true);
+
+            getMethod.ShouldNotBeNull();
+            getMethod.ShouldBeVirtual();
+        }
+    }
+
+    extension(MethodInfo method)
+    {
+        public void ShouldBeAbstract()
+        {
+            method.IsAbstract.ShouldBeTrue();
+        }
+
+        public void ShouldBeVirtual()
+        {
+            method.IsVirtual.ShouldBeTrue();
+        }
+
+        public void ShouldHaveOneParameter<T>()
+        {
+            method.GetParameters().Length.ShouldBe(1);
+            method.GetParameters().First().ParameterType.ShouldBe<T>();
         }
     }
 }
