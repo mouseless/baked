6faa39c9dfa36ba8d93a164fdc879425d8891424
Sefer Mirza <sefermirza@gmail.com>
2026-03-15 16:13:17 +0300

    move extensions to own folder

--- core/src/Baked/Recipe/DataSource/DataSourceExtensions.cs
+++ core/src/Baked/Recipe/DataSource/DataSourceExtensions.cs
@@ -1,25 +1,13 @@
-﻿using Baked.Architecture;
+using Baked.Architecture;
 using Baked.Business;
 using Baked.Recipe.DataSource;
-using Baked.Recipe.Monolith;
 
 namespace Baked;
 
-public static class BakeExtensions
+public static class DataSourceExtensions
 {
     extension(Bake bake)
     {
-        public Application Monolith(
-            Func<BusinessConfigurator, IFeature<BusinessConfigurator>> business,
-            Action<MonolithRecipe>? options = default
-        )
-        {
-            var recipe = new MonolithRecipe.Run(business);
-            options?.Invoke(recipe);
-
-            return bake.Application(recipe.Apply);
-        }
-
         public Application DataSource(
             Func<BusinessConfigurator, IFeature<BusinessConfigurator>> business,
             Action<DataSourceRecipe>? options = default
---
+++ core/src/Baked/Recipe/Monolith/MonolithExtensions.cs
@@ -0,0 +1,22 @@
+using Baked.Architecture;
+using Baked.Business;
+using Baked.Recipe.Monolith;
+
+namespace Baked;
+
+public static class MonolithExtensions
+{
+    extension(Bake bake)
+    {
+        public Application Monolith(
+            Func<BusinessConfigurator, IFeature<BusinessConfigurator>> business,
+            Action<MonolithRecipe>? options = default
+        )
+        {
+            var recipe = new MonolithRecipe.Run(business);
+            options?.Invoke(recipe);
+
+            return bake.Application(recipe.Apply);
+        }
+    }
+}
