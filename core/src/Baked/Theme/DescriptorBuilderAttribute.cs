ab54c7ae62b8b9465440481df95e0efa74e36f87
cihandeniz <cihan@gcbrains.com>
2026-04-26 13:40:26 +0300

    fix minor self-review issue

--- core/src/Baked/Theme/DescriptorBuilderAttribute.cs
+++ core/src/Baked/Theme/DescriptorBuilderAttribute.cs
@@ -1,8 +1,7 @@
 namespace Baked.Theme;
 
 [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
-public class DescriptorBuilderAttribute<T>
-    : Attribute, IComponentContextBasedBuilder<T>, IComponentContextFilter
+public class DescriptorBuilderAttribute<T> : Attribute, IComponentContextBasedBuilder<T>, IComponentContextFilter
 {
     public Func<ComponentContext, T> Builder { get; set; } = _ => throw DiagnosticCode.InvalidState.Exception($"`Builder` is required to be set for a descriptor, but not set to this instance.");
     public Func<ComponentContext, bool> Filter { get; set; } = cc => true;
