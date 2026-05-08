26c22fb73949a7991cd3d0e426ac7bb9462ed0f8
Liva <ahmetlivacengiz@gmail.com>
2026-05-08 13:04:04 +0300

    update dataContainer style

--- ui/src/runtime/components/PageSize.vue
+++ ui/src/runtime/components/PageSize.vue
@@ -5,7 +5,7 @@
     :schema
     :data
     class="
-      mr-2 shadow-none rounded-md
+      shadow-none rounded-md
       bg-slate-100 border-slate-100
       dark:bg-zinc-950 dark:border-zinc-950
     "
@@ -25,9 +25,6 @@ const model = defineModel({ type: null, required: true });
 </script>
 <style>
 .b-component--PageSize {
-  .p-select {
-    @apply border-none
-  }
   .p-select-label {
     font-size: inherit;
   }
