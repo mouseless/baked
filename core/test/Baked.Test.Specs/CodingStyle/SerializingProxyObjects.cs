9923a795174701fafbdb44d57cd1f3c800e73e65
dncsvr <disever10@alm.ku.edu.tr>
2026-05-18 11:21:06 +0300

    begin adding test for proxy object serialization

---
+++ core/test/Baked.Test.Specs/CodingStyle/SerializingProxyObjects.cs
@@ -0,0 +1,18 @@
+﻿namespace Baked.Test.CodingStyle;
+
+public class SerializingProxyObjects : TestNfr
+{
+    [TestCase]
+    public async Task Fetch_parents_false_returns_only_id_props_filled()
+    {
+        var response = await Client.GetAsync("/children/first?fetchParents=false");
+        dynamic? child = await response.Content.Deserialize();
+
+        object? actual = child?.@interface;
+
+        actual?.ShouldDeeplyBe(new Dictionary<string, object>()
+        {
+            { "$type", "Baked.Playground.Orm.Parent" }
+        });
+    }
+}
