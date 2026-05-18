907f762fac00435bb14648e2ff6e5c5c7eda678e
dncsvr <disever10@alm.ku.edu.tr>
2026-05-18 13:08:15 +0300

    complete proxy serialization test

--- core/test/Baked.Test.Specs/CodingStyle/SerializingProxyObjects.cs
+++ core/test/Baked.Test.Specs/CodingStyle/SerializingProxyObjects.cs
@@ -5,14 +5,42 @@ public class SerializingProxyObjects : TestNfr
     [TestCase]
     public async Task Fetch_parents_false_returns_only_id_props_filled()
     {
+        var parent = await Client.PostParents(name: "parent", surname: "test");
+        await Client.PostParentsChildren(id: (object)parent.id);
+
         var response = await Client.GetAsync("/children/first?fetchParents=false");
         dynamic? child = await response.Content.Deserialize();
 
-        object? actual = child?.@interface;
-
-        actual?.ShouldDeeplyBe(new Dictionary<string, object>()
+        object? childParent = child?.parent;
+        childParent.ShouldNotBeNull();
+        childParent.ShouldDeeplyBe(new Dictionary<string, object?>()
         {
-            { "$type", "Baked.Playground.Orm.Parent" }
+            { "id", parent.id }
+        });
+
+        object? wrappedParent = child?.parentWrapper.parent;
+        wrappedParent.ShouldNotBeNull();
+        wrappedParent.ShouldDeeplyBe(new Dictionary<string, object?>()
+        {
+            { "id", parent.id },
+            { "name", default },
+            { "description", default },
+            { "status", default },
+            { "role", default },
+            { "surname", default },
+        });
+
+        object? parentInterface = child?.parentInterface;
+        parentInterface.ShouldNotBeNull();
+        parentInterface?.ShouldDeeplyBe(new Dictionary<string, object?>()
+        {
+            { "$type", "Baked.Playground.Orm.Parent, Baked.Playground" },
+            { "id", parent.id },
+            { "name", default },
+            { "description", default },
+            { "status", default },
+            { "role", default },
+            { "surname", default },
         });
     }
 }
