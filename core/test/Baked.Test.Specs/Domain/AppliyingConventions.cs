ef27d5c69211471b94aed1c5ae91655c48b5c359
dncsvr <disever10@alm.ku.edu.tr>
2026-05-25 12:42:48 +0300

    add additional test cases to applying conventions

--- core/test/Baked.Test.Specs/Domain/AppliyingConventions.cs
+++ core/test/Baked.Test.Specs/Domain/AppliyingConventions.cs
@@ -35,7 +35,31 @@ public class AppliyingConventions : TestSpec
     }
 
     [Test]
-    public void Conventions_are_applied_based_on_levels()
+    public void Conventions_are_applied_based_on_added_order()
+    {
+        _builderOptions.Conventions.Add(new TestConvention("C1"));
+        _builderOptions.Conventions.Add(new TestConvention("C2"));
+
+        _postBuilder.EndBuild();
+
+        _values[0].ShouldBe("C1");
+        _values[1].ShouldBe("C2");
+    }
+
+    [Test]
+    public void Conventions_can_have_specific_orders()
+    {
+        _builderOptions.Conventions.Add(new TestConvention("C1"), order: 2);
+        _builderOptions.Conventions.Add(new TestConvention("C2"), order: 1);
+
+        _postBuilder.EndBuild();
+
+        _values[0].ShouldBe("C2");
+        _values[1].ShouldBe("C1");
+    }
+
+    [Test]
+    public void Conventions_can_have_level_based_orders()
     {
         _builderOptions.ConventionLevels.Add(new("A"));
         _builderOptions.ConventionLevels.Add(new("B"));
@@ -50,7 +74,38 @@ public class AppliyingConventions : TestSpec
     }
 
     [Test]
-    public void Levels_have_min_and_max_values()
+    public void Levels_are_applied_after_default_order()
+    {
+        _builderOptions.ConventionLevels.Add(new("A"));
+
+        _builderOptions.Conventions.Add(new TestConvention("C2"), order: builder => builder.Levels("A"));
+        _builderOptions.Conventions.Add(new TestConvention("C3"));
+
+        _postBuilder.EndBuild();
+
+        _values[0].ShouldBe("C3");
+        _values[1].ShouldBe("C2");
+    }
+
+    [Test]
+    public void Pre_levels_apply_before_default_order()
+    {
+        _builderOptions.ConventionLevels.Add(new("A"));
+        _builderOptions.ConventionLevels.Add(new("B", Pre: true));
+
+        _builderOptions.Conventions.Add(new TestConvention("C2"), order: builder => builder.Levels("A"));
+        _builderOptions.Conventions.Add(new TestConvention("C3"), order: builder => builder.Levels("B"));
+        _builderOptions.Conventions.Add(new TestConvention("C1"));
+
+        _postBuilder.EndBuild();
+
+        _values[0].ShouldBe("C3");
+        _values[1].ShouldBe("C1");
+        _values[2].ShouldBe("C2");
+    }
+
+    [Test]
+    public void Levels_have_default__min_and_max_values()
     {
         _builderOptions.ConventionLevels.Add(new("A"));
 
