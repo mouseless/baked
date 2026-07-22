390d5696747d6c6c1d78a83af3601c48206a2553
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2026-07-22 12:33:10 +0300

    remove anybyxx are unique test

--- core/test/Baked.Test.Specs/CodingStyle/ConfiguringUniqueColumns.cs
+++ core/test/Baked.Test.Specs/CodingStyle/ConfiguringUniqueColumns.cs
@@ -1,5 +1,4 @@
 using Baked.Playground.CodingStyle.EntitySubclass;
-using Baked.Playground.Orm;
 
 using NHConfiguration = NHibernate.Cfg.Configuration;
 
@@ -7,18 +6,6 @@ namespace Baked.Test.CodingStyle;
 
 public class ConfiguringUniqueColumns : TestSpec
 {
-    [Test]
-    public void Any_by_query_with_a_single_parameter_causes_corresponding_property_to_be_unique()
-    {
-        var configuration = GiveMe.The<NHConfiguration>();
-
-        var mapping = configuration.GetClassMapping(typeof(Entity));
-        var property = configuration.GetClassMapping(typeof(Entity)).PropertyIterator.FirstOrDefault(p => p.Name == nameof(Entity.Enum));
-        var column = property?.ColumnIterator.OfType<NHibernate.Mapping.Column>().FirstOrDefault();
-
-        column?.Unique.ShouldBeTrue();
-    }
-
     [Test]
     public void Single_by_query_with_a_single_parameter_causes_corresponding_property_to_be_unique()
     {
